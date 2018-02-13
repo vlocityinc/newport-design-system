8ed125fed831a7d8f85f619ef9c391642a238e62
Alejandro Lopez <alejandrolopez@salesforce.com>
2018-02-13 12:29:45 -0800

    Added a set util function. This is similar to lodash set much more simplistic without the crazy path parsing.
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000004c6osIAA/view
    
    
    @rev nikhil.kataria@
    
    #review-15266230 @*nikhil.katarai @aaron.liebling @j.garciarovetta @j.david
    BLT: CLI local=false
    
    @precheckin 15301889@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15302746]

--- packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/__tests__/objectMutation.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/__tests__/objectMutation.test.js
@@ -1,4 +1,4 @@
-import { pick, omit, updateProperties} from '../objectMutation';
+import { pick, omit, set, updateProperties, stringToPath} from '../objectMutation';
 
 const obj = {
     name: 'ass1',
@@ -6,13 +6,190 @@ const obj = {
     description: 'desc 1'
 };
 
+const assignmentItem = {
+    lhs: {
+        menuData: ['testmenudata'],
+        value: 'testvalue',
+    },
+    operator: {
+        value: 'add',
+    },
+    rhs: {
+        value: 'defaultrhs',
+    }
+};
+
+// our mock assignment with an array of assignment items
+const mockAssignment = {
+    items: [
+        {
+            lhs: {
+                menuData: ['firstmenudata'],
+                value: 'firstlhs',
+            },
+            operator: {
+                value: 'add',
+            },
+            rhs: {
+                value: 'firstrhs',
+            }
+        },
+        {
+            lhs: {
+                menuData: ['secondmenudata'],
+                value: 'secondlhs',
+            },
+            operator: {
+                value: 'setTo',
+            },
+            rhs: {
+                value: 'secondrhs',
+            }
+        },
+    ],
+    label: 'mockassignment',
+};
+
 const updatedProps = {
     name: 'ass2',
-    createdBy: 'user1'
+    createdBy: 'user1',
 };
 
 const allowedProps = ['name', 'label'];
 const omitProps = ['description'];
+
+describe('Set function', () => {
+    // test suite for the helper function that converts a string to a path array
+    describe('stringToPath helper function', () => {
+        it('should return the given path if already an array', () => {
+            const path = stringToPath(['testvalue']);
+            expect(path).toBeDefined();
+            expect(Array.isArray(path)).toBeTruthy();
+            expect(path[0]).toEqual('testvalue');
+        });
+
+        it('should return an empty array if not given an array or string', () => {
+            const path = stringToPath(42);
+            expect(Array.isArray(path)).toBeTruthy();
+            expect(path).toHaveLength(0);
+        });
+
+        it('should return a path given a period deliminated string with no indexing', () => {
+            const path = stringToPath('item.value.lhs');
+            expect(path).toHaveLength(3);
+            expect(path[0]).toEqual('item');
+            expect(path[1]).toEqual('value');
+            expect(path[2]).toEqual('lhs');
+        });
+
+        it('should return a path given a period deliminated string wth indexing', () => {
+            const path = stringToPath('item[0].value.lhs');
+            expect(path).toHaveLength(4);
+            expect(path[0]).toEqual('item');
+            expect(path[1]).toEqual('0');
+            expect(path[2]).toEqual('value');
+            expect(path[3]).toEqual('lhs');
+        });
+
+        it('should treat strings with non digits inside brackets as normal keys', () => {
+            const path = stringToPath('item[n].value');
+            expect(path).toHaveLength(2);
+            expect(path[0]).toEqual('item[n]');
+            expect(path[1]).toEqual('value');
+        });
+
+        it('should return a path with both non digit and digit indexes', () => {
+            const path = stringToPath('item[n][0].value');
+            expect(path).toHaveLength(3);
+            expect(path[0]).toEqual('item[n]');
+            expect(path[1]).toEqual('0');
+            expect(path[2]).toEqual('value');
+        });
+
+        it('should return a path with multiple digit indexing ', () => {
+            const path = stringToPath('item[0][1]');
+            expect(path).toHaveLength(3);
+            expect(path[0]).toEqual('item');
+            expect(path[1]).toEqual('0');
+            expect(path[2]).toEqual('1');
+        });
+    });
+
+    it('should be defined', () => {
+        expect(set).toBeDefined();
+    });
+
+    it('should return a new object', () => {
+        const newObj = set(assignmentItem, ['lhs', 'value'], 'newvalue');
+        expect(newObj).not.toBe(assignmentItem);
+    });
+
+    it('should accept a period deliminated string path', () => {
+        expect(() => {
+            set(assignmentItem, 'lhs.value', 'newvalue');
+        }).not.toThrow();
+    });
+
+    it('should update the value at the given path using string path', () => {
+        const newObj = set(assignmentItem, 'lhs.value', 'newvalue');
+        expect(newObj.lhs.value).toEqual('newvalue');
+    });
+
+    it('should update the value with a string path using indexing', () => {
+        const newObj = set(mockAssignment, 'items[0].lhs.value', 'newvalue');
+        expect(newObj.items[0].lhs.value).toEqual('newvalue');
+    });
+
+    it('should update the value at the given path', () => {
+        const newObj = set(assignmentItem, ['lhs', 'value'], 'newvalue');
+        expect(newObj.lhs.value).toEqual('newvalue');
+    });
+
+    it('should update the value at the given path using idexing', () => {
+        const newObj = set(mockAssignment, ['items', '0', 'lhs', 'value'], 'newvalue');
+        expect(newObj.items[0].lhs.value).toEqual('newvalue');
+    });
+
+    it('should return an object with keys from the given path if such a path does not already exist', () => {
+        const newObj = set(assignmentItem, ['firstNewKey', 'secondNewKey'], 'newvalue');
+        expect(newObj.firstNewKey.secondNewKey).toEqual('newvalue');
+    });
+
+    it('should only change the value at the given path', () => {
+        const newObj = set(assignmentItem, ['lhs', 'value'], 'newvalue');
+        expect(newObj.lhs.menudata).toEqual(assignmentItem.lhs.menudata);
+        expect(newObj.operator).toBeDefined();
+        expect(newObj.operator).toEqual(assignmentItem.operator);
+        expect(newObj.rhs).toBeDefined();
+        expect(newObj.rhs).toEqual(assignmentItem.rhs);
+    });
+
+    it('should only change the value at the given path using indexing', () => {
+        const newObj = set(mockAssignment, ['items', '0', 'lhs', 'value'], 'newvalue');
+        expect(newObj.items[0].lhs.menudata).toEqual(mockAssignment.items[0].lhs.menudata);
+        expect(newObj.items[0].operator).toEqual(mockAssignment.items[0].operator);
+        expect(newObj.items[0].rhs).toEqual(mockAssignment.items[0].rhs);
+        expect(newObj.items[1]).toEqual(mockAssignment.items[1]);
+    });
+
+    it('should return a clone of the object if no path is given', () => {
+        const newObj = set(assignmentItem, undefined, 'newvalue');
+        expect(newObj.lhs.value).toEqual('testvalue');
+    });
+
+    it('should set the value to undefined if no value is given', () => {
+        const newObj = set(assignmentItem, ['lhs', 'value'], undefined);
+        expect(newObj.lhs.value).toEqual(undefined);
+    });
+
+    it('should return a new object with keys from the given path if no object is given', () => {
+        const newObj = set(undefined, ['lhs', 'value'], 'newvalue');
+        expect(newObj).toBeDefined();
+        expect(newObj.lhs).toBeDefined();
+        expect(newObj.lhs.value).toBeDefined();
+        expect(newObj.lhs.value).toEqual('newvalue');
+    });
+});
 
 describe('Pick function', () => {
     it('should return new object', () => {
--- packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/objectMutation.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/objectMutation.js
@@ -1,4 +1,31 @@
 /**
+ * Helper method that set function will call.
+ * Recursively traverses an object based on the given path
+ * and uses Object.assign to update the property at the end of the path with
+ * the given value
+ * @param {Object}      obj   current object being ispected
+ * @param {Array}       path  current path
+ * @param {Object}      value value that will be set once end of path is reached
+ * @returns {Object}          the new object with the updated value
+ */
+const setValue = (obj, path, value) => {
+    if (path.length === 0) {
+        // base case:
+        // we do not have a path just return a clone of the object
+        return Object.assign({}, obj);
+    } else if (path.length === 1) {
+        // base case:
+        // we have reached the last key so we can now set the value to the desired key object pair
+        const key = path[0];
+        // make a new object with an updated property
+        return Object.assign({}, obj, { [key] : value });
+    }
+    // recursively call the set function and assign the results to our current object
+    const key = path[0];
+    return Object.assign({}, obj, { [key] : setValue(obj[key], path.slice(1), value) });
+};
+
+/**
  * Create a new object with updated properties
  * It can be used for creating a new object by calling the function without any argument.
  * @param {Object} obj existing object
@@ -8,6 +35,53 @@
 export function updateProperties(obj = {}, props = {}) {
     return Object.assign({}, obj, props);
 }
+
+/**
+ * Helper function that takes in a string path and converts to
+ * an array of keys
+ * NOTE: This assumes that you DO NOT have keys with notation keyName[digit]
+ * Instead any array bracket notation is assumed to be accessing an index in the
+ * value at the key position. So a string path with items[0].value would
+ * be converted into a path of ['items', '0', 'value' ]
+ * @param {String} string the string path we will convert to an array path
+ * @returns {Array}     array of keys that represent a path in an object
+ */
+export const stringToPath = (string) => {
+    if (Array.isArray(string)) {
+        // if we get an array we assume it is already a correct path
+        return string;
+    } else if (typeof string !== 'string') {
+        // if we get a non string path we just return an empty path array
+        return [];
+    }
+
+    // regex that matches string paths with [digit]. and [digit] pattern in them
+    const regex = /(\[)(\d)(\])(\.{0,1})/gi;
+    // replace all matching patterns with dot notation
+    const newString = string.replace(regex, (match, leftBracket, digit, rightBracket, rightPeriod) => {
+        return rightPeriod ? '.' + digit + '.' : '.' + digit;
+    });
+    // convert string path to array of keys
+    return newString.split('.');
+};
+
+/**
+ * Set the value of the object at the given path
+ * Does not mutate the given object
+ * This util is meant to be used by a function that needs to
+ * update a complex nested object in an immuatable way.
+ * If you just want to update a javascript object please
+ * use dot notation, that works much better.
+ * @param {Object}          obj   the object given
+ * @param {Array/String}    path  the path to the part of the object you want to update. Can be '.' period separated string path
+ * @param {Object}          value the value to be set at the path
+ * @returns {Object}              the new object with the updated vaue
+ */
+export const set = (obj = {}, path = [], value = undefined) => {
+    const keys = stringToPath(path);
+    const retObj = setValue(obj, keys, value);
+    return retObj;
+};
 
 /**
  * Create a new object with only allowed props.
