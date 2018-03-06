c211a469af5f6aa7593e454417d6cd9e020ae36f
Stephen Gesin <sgesin@salesforce.com>
2018-03-05 17:40:05 -0800

    Utilities for Popovers https://gus.my.salesforce.com/a07B0000004bH0W @rev ankush.bansal@ @markfixed@ BLT: CLI local=false
    @precheckin 15404714@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15405504]

--- packages/@flow-builder/ui/src/builder_platform_interaction/builder-utils/builder-utils.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/builder-utils/builder-utils.js
@@ -3,10 +3,16 @@ import { createComponent, dispatchGlobalEvent } from 'aura';
 import { STATE, ELEMENT_TYPE } from 'builder_platform_interaction-constant';
 
 /**
- * @constant
+ * @constant Panel type used for modals.
  * @type {string}
  */
 const MODAL = 'modal';
+
+/**
+ * @constant Panel type used for popovers.
+ * @type {string}
+ */
+const HOVER_PANEL = 'hoverPanel';
 
 /**
  * @constant
@@ -34,6 +40,12 @@ const elementTypeToConfigMap = {
         }
     }
 };
+
+/**
+ * @constant Tracks instances of created hover panels to help with management.
+ * @type {object}
+ */
+const hoverPanels = {};
 
 /**
  * @param {string} nodeType - String value to choose the actual component from the map,
@@ -71,3 +83,58 @@ export function invokePanel(cmpName,  attr) {
     });
 }
 
+/**
+ * Shows a popover/hoverPanel.
+ *
+ * Sample panelConfig for the left-panel hover:
+ * {
+ *     flavor: 'default',
+ *     'class': 'slds-popover_medium',
+ *     referenceElement: event.target,
+ *     enableAutoPlacement: false,
+ *     direction: 'east',
+ *     showCloseButton: false,
+ *     showPointer: true,
+ *     autoFocus: false,
+ *     closeOnClickOut: true,
+ *     enableFocusHoverPanelEventHandler: false
+ * }
+ *
+ * @param {string} cmpName Name of the component to be created for the popover body
+ * @param {object} attr Attributes values to use when creating an instance of cmpName
+ * @param {string} hoverId Identifier to use for the popover
+ * @param {object} panelConfig Attributes to use for the ui:createPanel event
+ */
+export function showHover(cmpName, attr, hoverId, panelConfig) {
+    createComponent(cmpName, attr, (newCmp, status) => {
+        if (status === STATE.SUCCESS) {
+            panelConfig.body = newCmp;
+
+            const config = {
+                panelType: HOVER_PANEL,
+                visible: true,
+                panelConfig,
+                onCreate: (hoverPanel) => {
+                    if (hoverPanel.isValid()) {
+                        hoverPanels[hoverId] = hoverPanel;
+                    }
+                },
+                onDestroy: () => {
+                    delete hoverPanels[hoverId];
+                }
+            };
+            dispatchGlobalEvent(UI_CREATE_PANEL, config);
+        }
+    });
+}
+
+/**
+ * Hides the popover/hoverPanel with the given hoverId.
+ * @param {string} hoverId Identifier of the popover to hide
+ */
+export function hideHover(hoverId) {
+    const hoverPanel = hoverPanels[hoverId];
+    if (hoverPanel) {
+        hoverPanel.requestClose();
+    }
+}
