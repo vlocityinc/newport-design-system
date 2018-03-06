1f94a6d617537da5417f29dd5aea1a4f549e9cb6
Amol Likhar <alikhar@salesforce.com>
2018-03-05 21:59:07 -0800

    Update search for grouped items and fix highlight regex not to use lookbehind.
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000004HRe8IAG/view
    
    #review-15367283
    
    @rev j.david@
    BLT: CLI local=false
    
    @precheckin 15406299@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15406491]

--- packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/__tests__/search.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/__tests__/search.test.js
@@ -1,58 +1,113 @@
-import { filterMatches } from '../search';
+import { filterMatches } from 'builder_platform_interaction-data-mutation-lib';
 
-const filterText = 'Name';
+const filterTextName = 'Name';
+const filterTextStr = 'Str';
+
 const menuData = [
     {
-        type: 'option-inline',
-        iconName: 'utility:search',
-        text: 'Create New Resource',
-        value: 'actionSearchAdvanced'
+        label: 'SObject Variables',
+        items: [
+            {
+                type: 'option-card',
+                text: 'FirstName',
+                iconName: 'standard:account',
+                subText: 'First Name',
+                value: 'MyAccount.FirstName'
+            },
+            {
+                type: 'option-card',
+                text: 'LastName',
+                iconName: 'standard:account',
+                subText: 'Last Name',
+                value: 'MyAccount.LastName'
+            },
+            {
+                type: 'option-card',
+                text: 'AnnualRevenue',
+                iconName: 'standard:account',
+                subText: 'Annual Revenue',
+                value: 'MyAccount.AnnualRevenue'
+            },
+        ]
     },
     {
-        type: 'option-card',
-        text: 'FirstName',
-        iconName: 'standard:account',
-        subText: 'First Name',
-        value: '{!MyAccount.FirstName}'
-    },
-    {
-        type: 'option-card',
-        text: 'LastName',
-        iconName: 'standard:account',
-        subText: 'Last Name',
-        value: '{!MyAccount.LastName}'
-    },
-    {
-        type: 'option-card',
-        text: 'AnnualRevenue',
-        iconName: 'standard:account',
-        subText: 'Annual Revenue',
-        value: '{!MyAccount.AnnualRevenue}'
+        label: 'Variables',
+        items: [
+            {
+                type: 'option-card',
+                text: 'MyName',
+                iconName: 'utility:text_color',
+                subText: 'String',
+                value: 'MyVar1'
+            },
+            {
+                type: 'option-card',
+                text: 'MyNumber2',
+                iconName: 'utility:number',
+                subText: 'Number',
+                value: 'MyNumber2'
+            },
+            {
+                type: 'option-card',
+                text: 'StartDateVar',
+                iconName: 'utility:date',
+                subText: 'Date',
+                value: 'StartDateVar'
+            },
+        ]
     }
 ];
 
-const filteredText = {
+const filteredTextName = {
     highlight: true,
     text: 'Name'
+};
+
+const filteredTextString = {
+    highlight: true,
+    text: 'Str'
 };
 
 describe('Combobox Search Library', () => {
     describe('Filter Matches', () => {
         it('Should return a new array.', () => {
-            const filteredArray = filterMatches(filterText, menuData);
+            const filteredArray = filterMatches(filterTextName, menuData);
             expect(filteredArray).not.toBe(menuData);
         });
 
-        it('New array should have length of 2.', () => {
-            const filteredArray = filterMatches(filterText, menuData);
+        it('New array should have 2 groups.', () => {
+            const filteredArray = filterMatches(filterTextName, menuData);
             expect(filteredArray).toHaveLength(2);
         });
 
-        it('New array should have filterText highlighted in text & subtext.', () => {
-            const filteredArray = filterMatches(filterText, menuData);
-            filteredArray.every(item => {
-                return expect(item.text).toContainEqual(filteredText) &&
-                    expect(item.subText).toContainEqual(filteredText);
+        it('New array should contain 2 items in first group.', () => {
+            const filteredArray = filterMatches(filterTextName, menuData);
+            expect(filteredArray[0].items).toHaveLength(2);
+        });
+
+        it('New array should contain 1 item in second group.', () => {
+            const filteredArray = filterMatches(filterTextName, menuData);
+            expect(filteredArray[1].items).toHaveLength(1);
+        });
+
+        it('New array should have filterText highlighted in text & subtext in group items.', () => {
+            const filteredArray = filterMatches(filterTextName, menuData);
+            filteredArray.forEach(group => {
+                group.items.every(item => {
+                    return expect(item.text).toContainEqual(filteredTextName) &&
+                        expect(item.subText).toContainEqual(filteredTextName);
+                });
+            });
+        });
+
+        it('Second search result should have highlight cleared from previous search.', () => {
+            let filteredArray = filterMatches(filterTextName, menuData);
+            filteredArray = filterMatches(filterTextStr, menuData);
+            filteredArray.forEach(group => {
+                group.items.every(item => {
+                    return expect(item.text).not.toContainEqual(filteredTextName) &&
+                        expect(item.subText).toContainEqual(filteredTextString);
+                });
             });
         });
     });
--- packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/search.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/data-mutation-lib/search.js
@@ -1,42 +1,40 @@
-import { deepCopy } from 'builder_platform_interaction-store-lib';
-
 /**
- * Filters the data passed in based on the text in a format that the combobox expects
- * @param {String} filterText - The value used to filter. This should just be the final string in an expression.
- * If the full value is '{!MyAccount.FirstN}', filterText should be 'FirstN'. If the full value is '{!MyAcc', filterText should be 'MyAcc'.
- * @param {Array} menuData - The menu data that needs to be filtered
- * @return {Array} The filtered and highlighted menu data
+ * Persist original text/subText else clear highlight if present.
+ * @param {Array} items - Menu Items to clear the highlighting from its text/subText
  */
-export function filterMatches(filterText, menuData) {
-    const matches = menuData.filter(menuItem => {
-        if (menuItem.type === 'heading') {
-            return false;
+function clearHighlight(items) {
+    items.forEach(item => {
+        if (!item.textNoHighlight) {
+            item.textNoHighlight = item.text;
+        } else if (hasHighlight(item.text)) {
+            item.text = item.textNoHighlight;
         }
-        return getIndex(filterText, menuItem.text) !== -1 || getIndex(filterText, menuItem.subtext) !== -1;
-    });
 
-    const formattedMatches = matches.map(menuItem => {
-        return createHighlightedItem(filterText, menuItem);
+        if (!item.subTextNoHighlight) {
+            item.subTextNoHighlight = item.subText;
+        } else if (hasHighlight(item.subText)) {
+            item.subText = item.subTextNoHighlight;
+        }
     });
-
-    return formattedMatches;
 }
 
 /**
- * Creates a copy of the item and adds the correct highlighting to text & subtext
- * @param {String} filterText - The value used to filter
- * @param {Object} item - One item that has matching values in text and/or subtext
- * @return {Object} The updated item with highlighted values
+ * Checks whether the text is highlighted. If it is an Array it is highlighted.
+ * @param {Array} text - The input text
+ * @returns {boolean} returns true if the text has highlight otherwise false
  */
-function createHighlightedItem(filterText, item) {
-    const formattedItem = deepCopy(item);
-    if (getIndex(filterText, formattedItem.text) !== -1) {
-        formattedItem.text = highlight(filterText, formattedItem.text);
-    }
-    if (getIndex(filterText, formattedItem.subText) !== -1) {
-        formattedItem.subText = highlight(filterText, formattedItem.subText);
-    }
-    return formattedItem;
+function hasHighlight(text) {
+    return Array.isArray(text);
+}
+
+/**
+ * Highlights the text and subText in the item.
+ * @param {String} filterText - The value used to filter
+ * @param {Object} item - One item that has matching values in text and/or subText
+ */
+function highlightItem(filterText, item) {
+    item.text = highlight(filterText, item.text);
+    item.subText = highlight(filterText, item.subText);
 }
 
 /**
@@ -46,8 +44,10 @@ function createHighlightedItem(filterText, item) {
  * @return {Array} The new array with highlighted text
  */
 function highlight(filterText, targetText) {
-    const regex = new RegExp('(?<=' + filterText + ')|(?=' + filterText + ')', 'i');
-    const targetTextArray = targetText.split(regex);
+    // lookbehind works only in V8 engine
+    // const regex = new RegExp('(?<=' + filterText + ')|(?=' + filterText + ')', 'i');
+    const regex = new RegExp('(' + filterText + ')', 'i');
+    const targetTextArray = targetText.split(regex).filter(String);
     const formattedText = targetTextArray.map(targetTextFragment => {
         return {
             highlight: (targetTextFragment.toLowerCase() === filterText.toLowerCase()),
@@ -68,4 +68,44 @@ function getIndex(filterText, targetText) {
         return targetText.toLowerCase().indexOf(filterText.toLowerCase());
     }
     return -1;
+}
+
+/**
+ * Filters the data passed in based on the text in a format that the combobox expects
+ * @param {String} filterText - The value used to filter. This should just be the final string in an expression.
+ * If the full value is '{!MyAccount.FirstN}', filterText should be 'FirstN'. If the full value is '{!MyAcc', filterText should be 'MyAcc'.
+ * @param {Array} menuData - The menu data that needs to be filtered
+ * @return {Array} The filtered and highlighted menu data
+ */
+export function filterMatches(filterText, menuData) {
+    if (!filterText || filterText.length === 0) {
+        throw new Error(`Filter text must not be undefined or empty but was ${filterText}`);
+    }
+
+    if (!Array.isArray(menuData)) {
+        throw new Error(`Menu data must be an array but was ${menuData}`);
+    }
+
+    const matchedGroups = [];
+    const countGroups = menuData.length;
+
+    for (let i = 0; i < countGroups; i++) {
+        clearHighlight(menuData[i].items || []);
+
+        const matchedGroupItems = menuData[i].items.filter(menuItem => {
+            return getIndex(filterText, menuItem.text) !== -1 || getIndex(filterText, menuItem.subText) !== -1;
+        });
+
+        // Only add group with matched items
+        if (matchedGroupItems && matchedGroupItems.length > 0) {
+            matchedGroupItems.forEach(menuItem => {
+                highlightItem(filterText, menuItem);
+            });
+            matchedGroups.push({
+                label: menuData[i].label,
+                items: matchedGroupItems
+            });
+        }
+    }
+    return matchedGroups;
 }
