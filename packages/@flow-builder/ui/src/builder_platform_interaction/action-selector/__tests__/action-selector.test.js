c1f02d5a403c37b69a6185892b7fbef199b4219b
Antoine Barsen <abarseni@salesforce.com>
2018-03-29 13:46:26 -0800

    Add jest test for Action Selector
    https://gus.my.salesforce.com/a07B0000004JUtHIAW
    
    @rev cchabanois@
    #review-15572049 @cchabanois
    @rtpcl:swarm 15566529@
    BLT: CLI local=false
    
    @precheckin 15570708@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15572048]

--- packages/@flow-builder/ui/src/builder_platform_interaction/action-selector/__tests__/action-selector.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/action-selector/__tests__/action-selector.test.js
@@ -65,43 +65,76 @@ describe('Action selector Tests', () => {
     beforeEach(() => {
         actionSelectorComponent = createComponentUnderTest();
     });
-    it('By default "Action" should be the selected Action type', () => {
-        const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
-        const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
-        expect(lightningCombobox.value).toBe(ELEMENT_TYPE.ACTION_CALL);
-        expect(groupedCombobox.items[0].items.map(item => item.text)).toEqual(["Post to Chatter", "Send Email"]);
-    });
-    it('Change selected value on the first combobox and check the value of the second combobox', () => {
-        const lightningCBChangeEventForApex = new CustomEvent('change', {detail: {value: ELEMENT_TYPE.APEX_CALL}});
-        const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
-        lightningCombobox.dispatchEvent(lightningCBChangeEventForApex);
-        return Promise.resolve().then(() => {
-            expect(lightningCombobox.value).toBe(ELEMENT_TYPE.APEX_CALL);
-            const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
-            expect(groupedCombobox.items[0].items.map(item => item.text)).toEqual(["Apex1", "Apex2", "Apex3"]);
-        });
-    });
-    it('ValueChangedEvent is fired when an action is selected', () => {
-        const interactionCombobox = actionSelectorComponent.querySelector(selectors.lightningInteractionCombobox);
-        return Promise.resolve().then(() => {
-            const eventCallback = jest.fn();
-            document.addEventListener(ValueChangedEvent.EVENT_NAME, eventCallback);
-            interactionCombobox.dispatchEvent(new ValueChangedEvent('emailSimple-emailSimple'));
-            expect(eventCallback).toHaveBeenCalled();
-            expect(eventCallback.mock.calls[0][0].detail.value).toMatchObject({actionName: "emailSimple", actionType: "emailSimple"});
-        });
-    });
     it('Action types without action instances are not displayed', () => {
         const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
         // No elements for "Email Alert"
         expect(lightningCombobox.options.map(option => option.label)).toEqual(["Action", "Apex"]);
     });
-    it('Action combobox label updated when action type is changed', () => {
-        const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
-        const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
-        lightningCombobox.dispatchEvent(new CustomEvent('change', {detail: {value: ELEMENT_TYPE.APEX_CALL}}));
-        return Promise.resolve().then(() => {
-            expect(groupedCombobox.label).toBe("Referenced Apex");
+    describe('By default "Action" should be the selected Action type', () => {
+        it('Other Action Type should be available', () => {
+            const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
+            const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
+            expect(lightningCombobox.value).toBe(ELEMENT_TYPE.ACTION_CALL);
+            expect(groupedCombobox.items[0].items.map(item => item.text)).toEqual(["Post to Chatter", "Send Email"]);
+        });
+        /*  TODO: uncomment when placeHolder will be implemented in action selector.
+         * it('Combobox placeholder should be : Find an Action...', () => {
+            const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
+            const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
+            expect(lightningCombobox.value).toBe(ELEMENT_TYPE.ACTION_CALL);
+            debugger;
+            expect(groupedCombobox.placeholder).toBe("Find an Action...");
+        });*/
+        it('Combobox Label should be : Referenced Action', () => {
+            const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
+            const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
+            expect(lightningCombobox.value).toBe(ELEMENT_TYPE.ACTION_CALL);
+            expect(groupedCombobox.label).toBe("Referenced Action");
+        });
+    });
+    describe('On action type changed', () => {
+        it('Change selected value on the first combobox and check the value of the second combobox', () => {
+            const lightningCBChangeEventForApex = new CustomEvent('change', {detail: {value: ELEMENT_TYPE.APEX_CALL}});
+            const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
+            lightningCombobox.dispatchEvent(lightningCBChangeEventForApex);
+            return Promise.resolve().then(() => {
+                expect(lightningCombobox.value).toBe(ELEMENT_TYPE.APEX_CALL);
+                const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
+                expect(groupedCombobox.items[0].items.map(item => item.text)).toEqual(["Apex1", "Apex2", "Apex3"]);
+            });
+        });
+        /* TODO: uncomment when placeHolder will be implemented in action selector.
+         * it('Action combobox placeholder updated', () => {
+            const lightningCBChangeEventForApex = new CustomEvent('change', {detail: {value: ELEMENT_TYPE.APEX_CALL}});
+            const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
+            lightningCombobox.dispatchEvent(lightningCBChangeEventForApex);
+            return Promise.resolve().then(() => {
+                expect(lightningCombobox.value).toBe(ELEMENT_TYPE.APEX_CALL);
+                const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningInteractionCombobox);
+                expect(groupedCombobox.placeholder).toBe("Find an Apex Class...");
+            });
+        });*/
+        it('Action combobox label updated', () => {
+            const lightningCBChangeEventForApex = new CustomEvent('change', {detail: {value: ELEMENT_TYPE.APEX_CALL}});
+            const lightningCombobox = actionSelectorComponent.querySelector(selectors.lightningCombobox);
+            lightningCombobox.dispatchEvent(lightningCBChangeEventForApex);
+            return Promise.resolve().then(() => {
+                expect(lightningCombobox.value).toBe(ELEMENT_TYPE.APEX_CALL);
+                const groupedCombobox = actionSelectorComponent.querySelector(selectors.lightningGroupedCombobox);
+                expect(groupedCombobox.label).toBe("Referenced Apex");
+            });
+        });
+    });
+    describe('On action selected', () => {
+        it('ValueChangedEvent is fired when an action is selected', () => {
+            const interactionCombobox = actionSelectorComponent.querySelector(selectors.lightningInteractionCombobox);
+            return Promise.resolve().then(() => {
+                const eventCallback = jest.fn();
+                document.addEventListener(ValueChangedEvent.EVENT_NAME, eventCallback);
+                interactionCombobox.dispatchEvent(new ValueChangedEvent('emailSimple-emailSimple'));
+                expect(eventCallback).toHaveBeenCalled();
+                expect(eventCallback.mock.calls[0][0].detail.value).toMatchObject({actionName: "emailSimple", actionType: "emailSimple"});
+            });
         });
     });
 });
