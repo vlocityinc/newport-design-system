1dd51279c2994aa9d651fcedb28526bef505145b
Cristina Nastasa <cnastasa@salesforce.com>
2018-04-18 17:08:59 -0800

    Add remove button to the outcome detail (in decision editor) https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000004XmnlIAC/view
    #review-15707848
    @rev aaron.liebling@
    
    @precheckin 15718345@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15719153]

--- packages/@flow-builder/ui/src/builder_platform_interaction/outcome/__tests__/outcome.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/outcome/__tests__/outcome.test.js
@@ -10,18 +10,16 @@ import {
 } from 'builder_platform_interaction-events';
 
 const outcomeWithOneConditional = {
-    guid: {
-        value: '123'
-    },
+    label: {value: 'Test Name of the Outcome'},
+    name: {value: 'Test Dev Name'},
+    guid: {value: '123'},
     conditionLogic: {value: '1'},
     conditions: [
         {name: 'condition1'}
     ]
 };
 const outcomeWithThreeConditionals = {
-    guid: {
-        value: '123'
-    },
+    guid: {value: '123'},
     conditionLogic: {value: '1 and 2'},
     conditions: [
         {name: 'condition1'},
@@ -32,6 +30,8 @@ const outcomeWithThreeConditionals = {
 
 const selectors = {
     row: 'builder_platform_interaction-row',
+    labelAndName: 'builder_platform_interaction-label-description',
+    button: 'lightning-button'
 };
 
 const createComponentUnderTest = () => {
@@ -44,212 +44,237 @@ const createComponentUnderTest = () => {
 };
 
 describe('Outcome', () => {
-    it('has one conditional row per conditional', () => {
-        const element = createComponentUnderTest();
-        element.outcome = outcomeWithThreeConditionals;
-
-        return Promise.resolve().then(() => {
-            const rowsArray = element.querySelectorAll(selectors.row);
-
-            expect(rowsArray).toHaveLength(3);
-        });
-    });
-    describe('delete button', () => {
-        it('is inactive if there is only one conditional', () => {
-            const element = createComponentUnderTest();
-            element.outcome = outcomeWithOneConditional;
-
-            return Promise.resolve().then(() => {
-                const row = element.querySelector(selectors.row);
-
-                expect(row.showDelete).toBeFalsy();
-            });
-        });
-        it('is active if there are multiple conditionals', () => {
-            const element = createComponentUnderTest();
-            element.outcome = outcomeWithThreeConditionals;
-
-            return Promise.resolve().then(() => {
-                const rows = element.querySelectorAll(selectors.row);
-
-                expect(rows[0].showDelete).toBeTruthy();
-                expect(rows[1].showDelete).toBeTruthy();
-                expect(rows[2].showDelete).toBeTruthy();
-            });
-        });
-    });
-    describe('prefix', () => {
-        it('show-prefix is always true', () => {
+    describe('advanced logic component', () => {
+        it('has one conditional row per conditional', () => {
             const element = createComponentUnderTest();
             element.outcome = outcomeWithThreeConditionals;
 
             return Promise.resolve().then(() => {
                 const rowsArray = element.querySelectorAll(selectors.row);
 
-                expect(rowsArray[0].showPrefix).toBeTruthy();
-                expect(rowsArray[1].showPrefix).toBeTruthy();
-                expect(rowsArray[2].showPrefix).toBeTruthy();
+                expect(rowsArray).toHaveLength(3);
             });
         });
-        describe('AND', () => {
-            it('displays no prefix for the first row', () => {
-                const rule = Object.assign({}, outcomeWithThreeConditionals);
-                rule.conditionLogic = {
-                    value: 'and'
-                };
-
+        describe('delete button', () => {
+            it('is inactive if there is only one conditional', () => {
                 const element = createComponentUnderTest();
-                element.outcome = rule;
+                element.outcome = outcomeWithOneConditional;
 
                 return Promise.resolve().then(() => {
-                    const rowsArray = element.querySelectorAll(selectors.row);
+                    const row = element.querySelector(selectors.row);
 
-                    expect(rowsArray[0].itemPrefix).toEqual('');
+                    expect(row.showDelete).toBeFalsy();
                 });
             });
-            it('displays the prefix "AND" for all other rows', () => {
-                const rule = Object.assign({}, outcomeWithThreeConditionals);
-                rule.conditionLogic = {
-                    value: 'and'
-                };
-
+            it('is active if there are multiple conditionals', () => {
                 const element = createComponentUnderTest();
-
-                element.outcome = rule;
+                element.outcome = outcomeWithThreeConditionals;
 
                 return Promise.resolve().then(() => {
-                    const rowsArray = element.querySelectorAll(selectors.row);
+                    const rows = element.querySelectorAll(selectors.row);
 
-                    expect(rowsArray[1].itemPrefix).toEqual('AND');
-                    expect(rowsArray[2].itemPrefix).toEqual('AND');
+                    expect(rows[0].showDelete).toBeTruthy();
+                    expect(rows[1].showDelete).toBeTruthy();
+                    expect(rows[2].showDelete).toBeTruthy();
                 });
             });
         });
-        describe('OR', () => {
-            it('displays no prefix for the first row', () => {
-                const rule = Object.assign({}, outcomeWithThreeConditionals);
-                rule.conditionLogic = {
-                    value: 'or'
-                };
-
-                const element = createComponentUnderTest();
-                element.outcome = rule;
-
-                return Promise.resolve().then(() => {
-                    const rowsArray = element.querySelectorAll(selectors.row);
-
-                    expect(rowsArray[0].itemPrefix).toEqual('');
-                });
-            });
-            it('displays the prefix "OR" for all other rows', () => {
-                const rule = Object.assign({}, outcomeWithThreeConditionals);
-                rule.conditionLogic = {
-                    value: 'or'
-                };
-
-                const element = createComponentUnderTest();
-
-                element.outcome = rule;
-
-                return Promise.resolve().then(() => {
-                    const rowsArray = element.querySelectorAll(selectors.row);
-
-                    expect(rowsArray[1].itemPrefix).toEqual('OR');
-                    expect(rowsArray[2].itemPrefix).toEqual('OR');
-                });
-            });
-        });
-        describe('advanced', () => {
-            it('displays 1-based numerical prefix for all rows', () => {
+        describe('prefix', () => {
+            it('show-prefix is always true', () => {
                 const element = createComponentUnderTest();
                 element.outcome = outcomeWithThreeConditionals;
 
                 return Promise.resolve().then(() => {
                     const rowsArray = element.querySelectorAll(selectors.row);
 
-                    expect(rowsArray[0].itemPrefix).toEqual('1');
-                    expect(rowsArray[1].itemPrefix).toEqual('2');
-                    expect(rowsArray[2].itemPrefix).toEqual('3');
+                    expect(rowsArray[0].showPrefix).toBeTruthy();
+                    expect(rowsArray[1].showPrefix).toBeTruthy();
+                    expect(rowsArray[2].showPrefix).toBeTruthy();
+                });
+            });
+            describe('AND', () => {
+                it('displays no prefix for the first row', () => {
+                    const rule = Object.assign({}, outcomeWithThreeConditionals);
+                    rule.conditionLogic = {
+                        value: 'and'
+                    };
+
+                    const element = createComponentUnderTest();
+                    element.outcome = rule;
+
+                    return Promise.resolve().then(() => {
+                        const rowsArray = element.querySelectorAll(selectors.row);
+
+                        expect(rowsArray[0].itemPrefix).toEqual('');
+                    });
+                });
+                it('displays the prefix "AND" for all other rows', () => {
+                    const rule = Object.assign({}, outcomeWithThreeConditionals);
+                    rule.conditionLogic = {
+                        value: 'and'
+                    };
+
+                    const element = createComponentUnderTest();
+
+                    element.outcome = rule;
+
+                    return Promise.resolve().then(() => {
+                        const rowsArray = element.querySelectorAll(selectors.row);
+
+                        expect(rowsArray[1].itemPrefix).toEqual('AND');
+                        expect(rowsArray[2].itemPrefix).toEqual('AND');
+                    });
+                });
+            });
+            describe('OR', () => {
+                it('displays no prefix for the first row', () => {
+                    const rule = Object.assign({}, outcomeWithThreeConditionals);
+                    rule.conditionLogic = {
+                        value: 'or'
+                    };
+
+                    const element = createComponentUnderTest();
+                    element.outcome = rule;
+
+                    return Promise.resolve().then(() => {
+                        const rowsArray = element.querySelectorAll(selectors.row);
+
+                        expect(rowsArray[0].itemPrefix).toEqual('');
+                    });
+                });
+                it('displays the prefix "OR" for all other rows', () => {
+                    const rule = Object.assign({}, outcomeWithThreeConditionals);
+                    rule.conditionLogic = {
+                        value: 'or'
+                    };
+
+                    const element = createComponentUnderTest();
+
+                    element.outcome = rule;
+
+                    return Promise.resolve().then(() => {
+                        const rowsArray = element.querySelectorAll(selectors.row);
+
+                        expect(rowsArray[1].itemPrefix).toEqual('OR');
+                        expect(rowsArray[2].itemPrefix).toEqual('OR');
+                    });
+                });
+            });
+            describe('advanced', () => {
+                it('displays 1-based numerical prefix for all rows', () => {
+                    const element = createComponentUnderTest();
+                    element.outcome = outcomeWithThreeConditionals;
+
+                    return Promise.resolve().then(() => {
+                        const rowsArray = element.querySelectorAll(selectors.row);
+
+                        expect(rowsArray[0].itemPrefix).toEqual('1');
+                        expect(rowsArray[1].itemPrefix).toEqual('2');
+                        expect(rowsArray[2].itemPrefix).toEqual('3');
+                    });
+                });
+            });
+        });
+
+        describe('handleAddCondition', () => {
+            it('fires addConditionEvent with current outcome GUI', () => {
+                const element = createComponentUnderTest();
+                // TODO: Figure out a way around this.  Currently shortcutting the element.outcome setter and setting
+                // element.outcome directly so it will be available when we call the handler.  See handler call below
+                element.outcome = outcomeWithThreeConditionals;
+
+                return Promise.resolve().then(() => {
+                    const eventCallback = jest.fn();
+                    element.addEventListener(AddConditionEvent.EVENT_NAME, eventCallback);
+
+                    // TODO: Figure out a better way to access the handler rather than through the class prototype
+                    Outcome.prototype.handleAddCondition.call(element, new AddListItemEvent());
+
+                    expect(eventCallback).toHaveBeenCalled();
+                    expect(eventCallback.mock.calls[0][0]).toMatchObject({
+                        parentGUID: element.outcome.guid
+                    });
+                });
+            });
+        });
+
+        describe('handleDeleteCondition', () => {
+            it('fires deleteConditionEvent with outcome GUI and condition index', () => {
+                const indexToDelete = 300;
+
+                const element = createComponentUnderTest();
+                // TODO: Figure out a way around this.  Currently shortcutting the element.outcome setter and setting
+                // element.outcome directly so it will be available when we call the handler.  See handler call below
+                element.outcome = outcomeWithThreeConditionals;
+
+                return Promise.resolve().then(() => {
+                    const eventCallback = jest.fn();
+                    element.addEventListener(DeleteConditionEvent.EVENT_NAME, eventCallback);
+
+                    // TODO: Figure out a better way to access the handler rather than through the class prototype
+                    Outcome.prototype.handleDeleteCondition.call(element, new DeleteListItemEvent(indexToDelete));
+
+                    expect(eventCallback).toHaveBeenCalled();
+                    expect(eventCallback.mock.calls[0][0]).toMatchObject({
+                        parentGUID: element.outcome.guid,
+                        index: indexToDelete
+                    });
+                });
+            });
+        });
+
+        describe('handleUpdateCondition', () => {
+            it('fires updateConditionEvent with outcome GUI, condition index, a property name, value and error', () => {
+                const updateData = {
+                    index: 300,
+                    value: 'newVal',
+                };
+
+
+                const element = createComponentUnderTest();
+                // TODO: Figure out a way around this.  Currently shortcutting the element.outcome setter and setting
+                // element.outcome directly so it will be available when we call the handler.  See handler call below
+                element.outcome = outcomeWithThreeConditionals;
+
+                return Promise.resolve().then(() => {
+                    const eventCallback = jest.fn();
+                    element.addEventListener(UpdateConditionEvent.EVENT_NAME, eventCallback);
+
+                    // TODO: Figure out a better way to access the handler rather than through the class prototype
+                    Outcome.prototype.handleUpdateCondition.call(element, new UpdateListItemEvent(
+                        updateData.index,
+                        updateData.value));
+
+                    expect(eventCallback).toHaveBeenCalled();
+                    expect(eventCallback.mock.calls[0][0]).toMatchObject({
+                        parentGUID: element.outcome.guid,
+                        index: updateData.index,
+                        value: updateData.value,
+                    });
                 });
             });
         });
     });
-
-    describe('handleAddCondition', () => {
-        it('fires addConditionEvent with current outcome GUI', () => {
+    describe('header section', () => {
+        it('has name and api name component', () => {
             const element = createComponentUnderTest();
-            // TODO: Figure out a way around this.  Currently shortcutting the element.outcome setter and setting
-            // element.outcome directly so it will be available when we call the handler.  See handler call below
-            element.outcome = outcomeWithThreeConditionals;
+            element.outcome = outcomeWithOneConditional;
 
             return Promise.resolve().then(() => {
-                const eventCallback = jest.fn();
-                element.addEventListener(AddConditionEvent.EVENT_NAME, eventCallback);
-
-                // TODO: Figure out a better way to access the handler rather than through the class prototype
-                Outcome.prototype.handleAddCondition.call(element, new AddListItemEvent());
-
-                expect(eventCallback).toHaveBeenCalled();
-                expect(eventCallback.mock.calls[0][0]).toMatchObject({
-                    parentGUID: element.outcome.guid
-                });
+                const labelAndNameComponents = element.querySelectorAll(selectors.labelAndName);
+                expect(labelAndNameComponents).toHaveLength(1);
+                expect(labelAndNameComponents[0].devName.value).toBe(outcomeWithOneConditional.name.value);
+                expect(labelAndNameComponents[0].label.value).toBe(outcomeWithOneConditional.label.value);
             });
         });
-    });
-
-    describe('handleDeleteCondition', () => {
-        it('fires deleteConditionEvent with outcome GUI and condition index', () => {
-            const indexToDelete = 300;
-
+        it('has Remove button', () => {
             const element = createComponentUnderTest();
-            // TODO: Figure out a way around this.  Currently shortcutting the element.outcome setter and setting
-            // element.outcome directly so it will be available when we call the handler.  See handler call below
-            element.outcome = outcomeWithThreeConditionals;
+            element.outcome = outcomeWithOneConditional;
 
             return Promise.resolve().then(() => {
-                const eventCallback = jest.fn();
-                element.addEventListener(DeleteConditionEvent.EVENT_NAME, eventCallback);
-
-                // TODO: Figure out a better way to access the handler rather than through the class prototype
-                Outcome.prototype.handleDeleteCondition.call(element, new DeleteListItemEvent(indexToDelete));
-
-                expect(eventCallback).toHaveBeenCalled();
-                expect(eventCallback.mock.calls[0][0]).toMatchObject({
-                    parentGUID: element.outcome.guid,
-                    index: indexToDelete
-                });
-            });
-        });
-    });
-
-    describe('handleUpdateCondition', () => {
-        it('fires updateConditionEvent with outcome GUI, condition index, a property name, value and error', () => {
-            const updateData = {
-                index: 300,
-                value: 'newVal',
-            };
-
-
-            const element = createComponentUnderTest();
-            // TODO: Figure out a way around this.  Currently shortcutting the element.outcome setter and setting
-            // element.outcome directly so it will be available when we call the handler.  See handler call below
-            element.outcome = outcomeWithThreeConditionals;
-
-            return Promise.resolve().then(() => {
-                const eventCallback = jest.fn();
-                element.addEventListener(UpdateConditionEvent.EVENT_NAME, eventCallback);
-
-                // TODO: Figure out a better way to access the handler rather than through the class prototype
-                Outcome.prototype.handleUpdateCondition.call(element, new UpdateListItemEvent(
-                    updateData.index,
-                    updateData.value));
-
-                expect(eventCallback).toHaveBeenCalled();
-                expect(eventCallback.mock.calls[0][0]).toMatchObject({
-                    parentGUID: element.outcome.guid,
-                    index: updateData.index,
-                    value: updateData.value,
-                });
+                const removeButton = element.querySelectorAll(selectors.button)[0];
+                expect(removeButton.label).toBe('Remove');
+                expect(removeButton.title).toBe('Remove Outcome');
             });
         });
     });
