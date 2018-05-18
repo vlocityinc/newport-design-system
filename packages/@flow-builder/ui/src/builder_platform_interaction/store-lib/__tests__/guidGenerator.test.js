7370c3177e455f8ab79cef73b663e6953cc04083
Daniel Bomb Moreno <dbombinmoreno@salesforce.com>
2018-05-17 20:02:23 -0800

    W-4998656 - Test guidGenerator.test.js.Handles_nulls is failing along with 3 other tests.
    https://gus.my.salesforce.com/apex/adm_bugdetail?id=a07B00000058S3R&sfdc.override=1
    
    @rev mcoughlin@
    @markfixed@
    #review-15892422
    @rtpcl:swarm 15891165@
    BLT: CLI local=false
    
    @precheckin 15892164@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15892421]

--- packages/@flow-builder/ui/src/builder_platform_interaction/store-lib/__tests__/guidGenerator.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/store-lib/__tests__/guidGenerator.test.js
@@ -1,24 +1,26 @@
 import { generateGuid } from '../guidGenerator';
 
-let testCounter = 3;
+let testCounter = 1;
 const next = function () {
     return String(testCounter++);
 };
 
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
 describe('Guid Generator', () => {
     it('Handles nulls', () => {
-        expect(generateGuid(null)).toEqual('uid_0');
-        expect(generateGuid()).toEqual('uid_1');
+        expect(generateGuid(null)).toMatch(uuidRegex);
+        expect(generateGuid()).toMatch(uuidRegex);
     });
 
     it('Handles numbers', () => {
-        expect(generateGuid('123984')).toEqual('uid_2');
+        expect(generateGuid('123984')).toMatch('uid_0');
     });
 
     it('Increments', () => {
         let i = 0;
         for (i = 0; i < 100; ++i) {
-            expect(generateGuid()).toEqual('uid_' + next());
+            expect(generateGuid('uid')).toEqual('uid_' + next());
         }
     });
 
