854669f5826e36b2cda50d7be3acbd0aa80bc16f
Alejandro Lopez <alejandrolopez@salesforce.com>
2018-05-31 09:45:38 -0800

    tests for base resource picker component
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B000000595KcIAI/view
    
    @rev alikhar@
    
    #review-15927290
    BLT: CLI local=false
    
    @precheckin 15962387@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 15963271]

---
+++ packages/@flow-builder/ui/src/builder_platform_interaction/base-resource-picker/__tests__/base-resource-picker.test.js
@@ -0,0 +1,106 @@
+import { createElement } from 'engine';
+import { FLOW_DATA_TYPE } from 'builder_platform_interaction-data-type-lib';
+import { FilterMatchesEvent } from 'builder_platform_interaction-events';
+import { filterMatches } from 'builder_platform_interaction-expression-utils';
+import BaseResourcePicker from '../base-resource-picker';
+
+const setupComponentUnderTest = (props) => {
+    const element = createElement('builder_platform_interaction-resource-picker', {
+        is: BaseResourcePicker,
+    });
+    Object.assign(element, props);
+    document.body.appendChild(element);
+    return element;
+};
+
+const selectors = {
+    COMBOBOX: 'builder_platform_interaction-combobox',
+};
+
+const filteredMenuData = ['filteredMenuData'];
+
+jest.mock('builder_platform_interaction-expression-utils', () => {
+    return {
+        filterMatches: jest.fn().mockReturnValue(['filteredMenuData']).mockName('filterMatches'),
+    };
+});
+
+describe('base-resource-picker', () => {
+    const comboboxConfig = {
+        label: 'test label',
+        placeholder: 'test placeholder',
+        errorMessage: 'test error message',
+        literalsAllowed: true,
+        required: true,
+        disabled: false,
+        type: FLOW_DATA_TYPE.NUMBER.value,
+    };
+
+    it('has a static getComboboxConfig method that returns a ComboboxConfig object', () => {
+        expect(BaseResourcePicker.getComboboxConfig).toEqual(expect.any(Function));
+        expect(BaseResourcePicker.getComboboxConfig('', '', '', '', '', '', '')).toMatchObject(
+            {
+                label: expect.anything(),
+                placeholder: expect.anything(),
+                errorMessage: expect.anything(),
+                literalsAllowed: expect.anything(),
+                required: expect.anything(),
+                disabled: expect.anything(),
+                type: expect.anything(),
+            }
+        );
+    });
+
+    it('contains one flow combobox', () => {
+        const baseResourcePicker = setupComponentUnderTest({comboboxConfig});
+        return Promise.resolve().then(() => {
+            const flowCombobox = baseResourcePicker.querySelector(selectors.COMBOBOX);
+            expect(flowCombobox).toBeDefined();
+        });
+    });
+
+    it('initialized the combobox with the values inside the combobox config object', () => {
+        const baseResourcePicker = setupComponentUnderTest({comboboxConfig, displayText: 'test display text'});
+        return Promise.resolve().then(() => {
+            const flowCombobox = baseResourcePicker.querySelector(selectors.COMBOBOX);
+            expect(flowCombobox.label).toEqual(comboboxConfig.label);
+            expect(flowCombobox.placeholder).toEqual(comboboxConfig.placeholder);
+            expect(flowCombobox.literalsAllowed).toEqual(comboboxConfig.literalsAllowed);
+            expect(flowCombobox.required).toEqual(comboboxConfig.required);
+            expect(flowCombobox.disabled).toEqual(comboboxConfig.disabled);
+            expect(flowCombobox.type).toEqual(comboboxConfig.type);
+        });
+    });
+
+    it('can set the value of the flow combobox', () => {
+        const defaultValue = {value:'testVal', displayText:'test val'};
+        const baseResourcePicker = setupComponentUnderTest({comboboxConfig, value: defaultValue});
+        return Promise.resolve().then(() => {
+            const flowCombobox = baseResourcePicker.querySelector(selectors.COMBOBOX);
+            expect(flowCombobox.value).toEqual(defaultValue);
+        });
+    });
+
+    it('can set the displayText of the flow combobox', () => {
+        const defaultDisplayText = 'test display text';
+        const baseResourcePicker = setupComponentUnderTest({comboboxConfig, displayText: defaultDisplayText});
+        return Promise.resolve().then(() => {
+            const flowCombobox = baseResourcePicker.querySelector(selectors.COMBOBOX);
+            expect(flowCombobox.displayText).toEqual(defaultDisplayText);
+        });
+    });
+
+    describe('event handlers', () => {
+        it('handles the filter menu data event', () => {
+            const baseResourcePicker = setupComponentUnderTest({comboboxConfig, });
+            const fullMenuData = ['full menu data'];
+            baseResourcePicker.setMenuData(fullMenuData);
+            const flowCombobox = baseResourcePicker.querySelector(selectors.COMBOBOX);
+            flowCombobox.dispatchEvent(new FilterMatchesEvent('someValue'));
+            return Promise.resolve().then(() => {
+                expect(filterMatches).toHaveBeenCalledWith('someValue', fullMenuData);
+                expect(flowCombobox.menuData).toEqual(filteredMenuData);
+            });
+        });
+    });
+});
