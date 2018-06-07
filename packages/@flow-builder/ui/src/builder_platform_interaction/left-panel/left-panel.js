012d1dd19b359414905a601a9e855ee81e232c7f
Avinash Kasipathy <akasipathy@salesforce.com>
2018-06-06 19:55:22 -0800

    Delete - Canvas Elements from Resource Details Panel https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000004bH8VIAU/view
    #review-15997889
    @rev sgesin@
    @markfixed@
    BLT: CLI local=false
    
    @precheckin 16001013@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16001274]

--- packages/@flow-builder/ui/src/builder_platform_interaction/left-panel/left-panel.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/left-panel/left-panel.js
@@ -1,5 +1,5 @@
 import { Element, api, track } from 'engine';
-import { EditElementEvent } from 'builder_platform_interaction-events';
+import { EditElementEvent, CANVAS_EVENT } from 'builder_platform_interaction-events';
 import { resourceSectionsSelector } from 'builder_platform_interaction-selectors';
 import { Store } from 'builder_platform_interaction-store-lib';
 
@@ -7,6 +7,7 @@ import headerText from '@label/FlowBuilderLeftPanel.headerText';
 import elementTabText from '@label/FlowBuilderLeftPanel.elementTabText';
 import resourceTabText from '@label/FlowBuilderLeftPanel.resourceTabText';
 import editButtonLabel from '@label/FlowBuilderResourceDetailsPanel.editButtonLabel';
+import deleteButtonLabel from '@label/FlowBuilderResourceDetailsPanel.deleteButtonLabel';
 import backButtonAltText from '@label/FlowBuilderResourceDetailsPanel.backButtonAltText';
 
 const LABELS = {
@@ -14,6 +15,7 @@ const LABELS = {
     LEFT_PANEL_ELEMENT_TAB_TEXT: elementTabText,
     LEFT_PANEL_RESOURCE_TAB_TEXT: resourceTabText,
     RESOURCE_DETAILS_EDIT_BUTTON_TEXT: editButtonLabel,
+    RESOURCE_DETAILS_DELETE_BUTTON_TEXT: deleteButtonLabel,
     RESOURCE_DETAILS_BACK_BUTTON_ATL_TEXT: backButtonAltText
 };
 
@@ -21,6 +23,7 @@ const ACTIVETABID_DEFAULT = 'left-panel-tabitem-elements';
 
 let storeInstance;
 let unsubscribeStore;
+
 
 export default class LeftPanel extends Element {
     @track activetabid = ACTIVETABID_DEFAULT;
@@ -55,6 +58,11 @@ export default class LeftPanel extends Element {
         return LABELS;
     }
 
+    // TO DO: Remove this logic once we figure out how we are handling Non Canvas Elements Deletion
+    get showDeleteButton() {
+        return storeInstance.getCurrentState().elements[this.resourceDetails.GUID].isCanvasElement;
+    }
+
     handleTabChange(event) {
         this.activetabid = event.detail.tabId;
     }
@@ -82,6 +90,20 @@ export default class LeftPanel extends Element {
         const guid = this.resourceDetails.GUID;
         const editElementEvent = new EditElementEvent(guid);
         this.dispatchEvent(editElementEvent);
+    }
+
+    handleDeleteButtonClicked() {
+        this.showResourceDetailsPanel = false;
+        const selectedCanvasElementGUIDs = [this.resourceDetails.GUID];
+        const deleteEvent = new CustomEvent(CANVAS_EVENT.DELETE_ON_CANVAS, {
+            bubbles: true,
+            composed: true,
+            cancelable: true,
+            detail: {
+                selectedCanvasElementGUIDs
+            }
+        });
+        this.dispatchEvent(deleteEvent);
     }
 
     handleResourceDetailsBackLinkClicked() {
