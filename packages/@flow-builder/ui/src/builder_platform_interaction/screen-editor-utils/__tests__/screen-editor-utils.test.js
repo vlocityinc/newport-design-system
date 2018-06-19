31e9e2abeae5cceaa1a232db2272d3af8faf0f43
Raji Srikantan <rsrikantan@salesforce.com>
2018-06-18 16:27:20 -0800

    [216] FSB: Add some tests for screen-editor-util booleanAttributeValue function https://gus.my.salesforce.com/apex/adm_userstorydetail?id=a07B0000004vfPpIAI&sfdc.override=1 @rev dbombinmoreno@ #review-16062347 @*dbombinmoreno BLT: CLI local=false
    @precheckin 16064358@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16065337]

---
+++ packages/@flow-builder/ui/src/builder_platform_interaction/screen-editor-utils/__tests__/screen-editor-utils.test.js
@@ -0,0 +1,42 @@
+import {
+    booleanAttributeValue
+} from 'builder_platform_interaction-screen-editor-utils';
+
+const bar = 'bar';
+
+describe('booleanAttributeValue function', () => {
+    it('Returns true if the property value is true', () => {
+        const foo = {bar: true};
+        expect(booleanAttributeValue(foo, bar)).toBe(true);
+    });
+
+    it('Returns true if the property value is the string "true"', () => {
+        const foo = {bar: 'true'};
+        expect(booleanAttributeValue(foo, 'bar')).toBe(true);
+    });
+
+    it('Returns true if the property value is the same as property name', () => {
+        const foo = {bar};
+        expect(booleanAttributeValue(foo, bar)).toBe(true);
+    });
+
+    it('Returns false if the property value is a random string', () => {
+        const foo = {bar: 'someString'};
+        expect(booleanAttributeValue(foo, 'bar')).toBe(false);
+    });
+
+    it('Returns false if the property value is false', () => {
+        const foo = {bar: false};
+        expect(booleanAttributeValue(foo, 'bar')).toBe(false);
+    });
+
+    it('Returns undefined if the property value is undefined', () => {
+        const foo = {bar: undefined};
+        expect(booleanAttributeValue(foo, 'bar')).toBe(undefined);
+    });
+
+    it('Returns undefined if the property does not exist', () => {
+        const foo = {};
+        expect(booleanAttributeValue(foo, 'bar')).toBe(undefined);
+    });
+});
