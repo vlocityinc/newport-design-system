2a4d895982fd7c88a40e61b92828f28a0316072b
Nhi Nguyen <pham.nguyen@salesforce.com>
2018-07-12 05:53:44 -0800

    change header label + css https://gus.my.salesforce.com/apex/ADM_WorkView?id=a07B0000004vWHDIA2&sfdc.override=1 #review-16202346 @rev bdufourd@ BLT: CLI local=false
    @precheckin 16202365@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16202565]

--- packages/@flow-builder/ui/src/builder_platform_interaction/record-lookup-editor/record-lookup-editor-labels.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/record-lookup-editor/record-lookup-editor-labels.js
@@ -2,9 +2,11 @@
 import selectObject from '@label/FlowBuilderRecordEditor.selectObject';
 import object from '@label/FlowBuilderRecordEditor.object';
 import objectPlaceholder from '@label/FlowBuilderRecordEditor.objectPlaceholder';
+import recordLookupTitle from '@label/FlowBuilderRecordEditor.recordLookupTitle';
 
 export const LABELS = {
     selectObject,
     object,
-    objectPlaceholder
+    objectPlaceholder,
+    recordLookupTitle
 };
