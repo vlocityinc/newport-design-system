8d0f4981e1b04d58d69943dec490b9e84cea0313
Aniko van der Lee <avanderlee@salesforce.com>
2018-09-21 16:12:20 -0800

    Fixing some jest tests that are failing because loggingUtils isn't mocked https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005aM8iIAE/view @rev priya.mittal@ @markfixed@ @testfix@
    @precheckin 16635455@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16636444]

---
+++ packages/@flow-builder/ui/jest-modules/builder_platform_interaction/loggingUtils/loggingUtils.js
@@ -0,0 +1,23 @@
+﻿/**
+ * Mock object for the loggingUtils.  If a test needs specific values returned by any function in the library
+ * then the test should override those methods as needed.
+ */
+let mock = {
+        logPerfTransactionStart : jest.fn(),
+        logPerfTransactionEnd : jest.fn(),
+        logPerfMarkStart : jest.fn(),
+        logPerfMarkEnd : jest.fn(),
+    };
+
+export const logPerfTransactionStart = (arg) => {
+    return mock.logPerfTransactionStart(arg);
+};
+export const logPerfTransactionEnd = (arg) => {
+    return mock.logPerfTransactionEnd(arg);
+};
+export const logPerfMarkStart = (arg) => {
+    return mock.logPerfMarkStart(arg);
+};
+export const logPerfMarkEnd = (arg) => {
+    return mock.logPerfMarkEnd(arg);
+};
