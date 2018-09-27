0f59b0af90c88a49363c003e6ef6909a623c9d4a
ankush.bansal <a@b>
2018-09-27 14:56:09 -0800

    [218][User story] Formula factory cleanup and unit tests
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005aYyAIAU/view
    
    #review-16661174 @*nikhil.kataria @cchabanois
    
    @rev nikhil.kataria@
    BLT: CLI local=false
    
    @precheckin 16662222@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16663031]

---
+++ packages/@flow-builder/ui/src/builder_platform_interaction/elementFactory/__tests__/formula.test.js
@@ -0,0 +1,52 @@
+import { createFormula, createFormulaForStore, createFormulaMetadataObject } from '../formula';
+
+const formulaMetadata = {
+    dataType: 'String',
+    expression: 'Hello {!guid}',
+    name: 'Formula1',
+    scale: 0
+};
+
+const formula = {
+    name: 'Formula1',
+    description: '',
+    expression: 'Hello {!guid}',
+    dataType: 'String',
+    scale: 0,
+    elementType: 'FORMULA'
+};
+
+describe('Formula:', () => {
+    describe('createFormula function', () => {
+        it('returns a new formula object when no argument is passed', () => {
+            const expectedResult = {
+                name: '',
+                description: '',
+                expression: '',
+                dataType: null,
+                scale: 2,
+                elementType: 'FORMULA'
+            };
+            const actualResult = createFormula();
+            expect(actualResult).toMatchObject(expectedResult);
+        });
+        it('returns a new formula object with same value when an existing formula is passed', () => {
+            const actualResult = createFormula(formula);
+            expect(actualResult).toMatchObject(formula);
+        });
+    });
+    describe('createFormulaForStore function', () => {
+        it('return a new formula object in shape of store when formula metadata is passed', () => {
+            const { elements } = createFormulaForStore(formulaMetadata);
+            expect(Object.values(elements)[0]).toMatchObject(formula);
+        });
+    });
+    describe('createFormulaMetadata function', () => {
+        it('return a new metadata object when variable has default value of type string value', () => {
+            const actualResult = createFormulaMetadataObject(formula);
+            expect(actualResult).toMatchObject(formulaMetadata);
+        });
+    });
+});
+
+
--- packages/@flow-builder/ui/src/builder_platform_interaction/elementFactory/formula.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/elementFactory/formula.js
@@ -1,37 +1,59 @@
-import { ELEMENT_TYPE } from "builder_platform_interaction/flowMetadata";
-import { baseResource, baseElementsArrayToMap } from "./base/baseElement";
-import { baseResourceMetadataObject } from "./base/baseMetadata";
+import { ELEMENT_TYPE } from 'builder_platform_interaction/flowMetadata';
+import { baseResource, baseElementsArrayToMap } from './base/baseElement';
+import { baseResourceMetadataObject } from './base/baseMetadata';
 
 const elementType = ELEMENT_TYPE.FORMULA;
 
+/**
+ * Either creates a new formula or create a new copy of existing formula
+ * @param {Object} formula existing formula which needs to be copied
+ * @return {Object} newFormula new formula which is created
+ */
 export function createFormula(formula = {}) {
     const newFormula = baseResource(formula);
-    const { expression = '', dataType = null, scale = 2 } = formula;
-    return Object.assign(newFormula, {
+    const {
+        expression = '',
+        dataType = null,
+        scale = 2
+    } = formula;
+    Object.assign(newFormula, {
         expression,
         dataType,
         scale,
         elementType
     });
+    return newFormula;
 }
 
+/**
+ * Create a new copy of existing formula in shape as expected by store.
+ * @param {Object} formula existing formula which needs to be copied
+ * @return {Object} Map containing guid as key and new formula as value
+ */
 export function createFormulaForStore(formula) {
     const newFormula = createFormula(formula);
-
     return baseElementsArrayToMap([newFormula]);
 }
 
+/**
+ * Create a new copy of existing formula in shape as expected by flow metadata.
+ * @param {Object} formula existing formula which needs to be copied
+ * @return {Object} newFormula new formula which is created
+ */
 export function createFormulaMetadataObject(formula) {
     if (!formula) {
         throw new Error('formula is not defined');
     }
-
     const newFormula = baseResourceMetadataObject(formula);
-    const { expression, dataType, scale } = formula;
-
-    return Object.assign(newFormula, {
+    const {
+        expression,
+        dataType,
+        scale
+    } = formula;
+    Object.assign(newFormula, {
         expression,
         dataType,
         scale
     });
+    return newFormula;
 }
