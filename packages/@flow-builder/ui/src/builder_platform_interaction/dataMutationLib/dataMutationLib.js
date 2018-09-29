abe60302427a9e21d1dcd43deef121d6df88699d
ankush.bansal <a@b>
2018-09-28 17:36:03 -0800

    [218][User story] Removing element mutation and demutation from data mutation lib
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005aYyAIAU/view
    
    #review-16669198 @*nikhil.kataria
    
    @rev nikhil.kataria@ async
    BLT: CLI local=false
    
    @precheckin 16669338@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16669696]

--- packages/@flow-builder/ui/src/builder_platform_interaction/dataMutationLib/dataMutationLib.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/dataMutationLib/dataMutationLib.js
@@ -1,10 +1,7 @@
 export * from './arrayMutation';
 export * from './objectMutation';
 export * from './elementDataMutation';
-export * from './propertyEditorDataMutation';
 export * from './stringMutation';
 export * from './screenEditorDataMutation';
 export * from './mergeFieldsMutation';
-export { mutateOutcome } from './decisionEditorDataMutation';
 export { GUID_SUFFIX, mutateFEROV, deMutateFEROV } from './ferovEditorDataMutation';
-export { FEROV_DATA_TYPE_PROPERTY } from './variableConstantEditorDataMutation';
