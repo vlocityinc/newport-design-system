c2245afd5e82c684b516d12840d08793d2c661e1
Raji Srikantan <rsrikantan@salesforce.com>
2018-10-01 18:02:30 -0800

    [218] Wait/Platform Event - UI for setting output variable https://gus.my.salesforce.com/a07B0000005YnLKIA0 @rev j.david, alikhar@ #review-16669238 @adeogirikar, @alejandrolopez, @aaron.liebling, @j.david, @cnastasa, @j.garciarovetta BLT: CLI local=false
    @precheckin 16679438@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16680004]

--- packages/@flow-builder/ui/src/builder_platform_interaction/waitPlatformEvent/waitPlatformEventLabels.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/waitPlatformEvent/waitPlatformEventLabels.js
@@ -2,10 +2,14 @@ import eventLabel from '@salesforce/label/FlowBuilderWaitEditor.eventLabel';
 import selectEventLabel from '@salesforce/label/FlowBuilderWaitEditor.selectEventLabel';
 import filterEventLabel from '@salesforce/label/FlowBuilderWaitEditor.filterEventLabel';
 import assignOutputsToVariablesLabel from '@salesforce/label/FlowBuilderWaitEditor.assignOutputsToVariablesLabel';
+import platformEventOutputHelpText from '@salesforce/label/FlowBuilderWaitEditor.platformEventOutputHelpText';
+import platformEventOutputLabel from '@salesforce/label/FlowBuilderWaitEditor.platformEventOutputLabel';
 
 export const LABELS = {
     eventLabel,
     selectEventLabel,
     filterEventLabel,
-    assignOutputsToVariablesLabel
+    assignOutputsToVariablesLabel,
+    platformEventOutputLabel,
+    platformEventOutputHelpText,
 };
