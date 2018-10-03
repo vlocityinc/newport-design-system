e3ca616cd42ca5a7faeefc4f74aeafbefbc2a635
Cristina Nastasa <cnastasa@salesforce.com>
2018-10-03 10:51:20 -0800

    Text Template Editor validation: text area cannot be empty https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005YuBuIAK/view
    #review-16686396
    @rev rsrikantan@
    
    @precheckin 16690579@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16691983]

--- packages/@flow-builder/ui/src/builder_platform_interaction/textTemplateEditor/textTemplateValidation.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/textTemplateEditor/textTemplateValidation.js
@@ -1,3 +1,11 @@
+import * as ValidationRules from "builder_platform_interaction/validationRules";
 import { Validation } from 'builder_platform_interaction/validation';
 
-export const textTemplateValidation = new Validation();
+export const additionalRules = {
+        text : [
+            ValidationRules.shouldNotBeBlank,
+            ValidationRules.shouldNotBeNullOrUndefined
+        ]
+    };
+
+export const textTemplateValidation = new Validation(additionalRules);
