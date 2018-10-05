0c4aa92725beb7518c1832f1e4f83875f5c825b5
Amar Gajanan Deogirikar <adeogirikar@salesforce.com>
2018-10-05 15:58:55 -0800

    tests for defaultPath @rev aaron.liebling@ https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000004iQ5iIAE/view BLT: CLI local=false
    @precheckin 16710375@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16711328]

--- packages/@flow-builder/ui/src/builder_platform_interaction/waitEditor/__tests__/waitEditor.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/waitEditor/__tests__/waitEditor.test.js
@@ -19,9 +19,13 @@ function createComponentForTest(props) {
     return el;
 }
 
+const DEFAULT_WAIT_EVENT_ID = 'defaultWaitEvent';
+
 const selectors = {
     LABEL_DESCRIPTION: 'builder_platform_interaction-label-description',
     WAIT_EVENT: 'builder_platform_interaction-wait-event',
+    DEFAULT_PATH: 'builder_platform_interaction-label-description.defaultPath',
+    REORDERABLE_NAV: 'builder_platform_interaction-reorderable-vertical-navigation',
 };
 
 jest.mock('../waitReducer', () => {
@@ -30,29 +34,31 @@ jest.mock('../waitReducer', () => {
     };
 });
 
+let noErrorState;
+beforeEach(() => {
+    noErrorState = {
+        name : {value: 'testWaitName', error: null},
+        label : {value: 'testWaitLabel', error: null},
+        description: 'wait description',
+        elementType : 'WAIT',
+        guid : '141f916fee-1c6f3-108bf-1ca54-16c041fcba152a7',
+        isCanvasElement : true,
+        defaultConnectorLabel: { value: 'defaultConnectorLabelValue'},
+        locationX : 789,
+        locationY : 123,
+        waitEvents: [
+            {
+                guid: 'waitEvent1',
+                label: 'wait event label',
+                name: 'waitEventName',
+                conditions: [],
+                conditionLogic: { value: CONDITION_LOGIC.AND, error: null },
+            }
+        ]
+    };
+});
+
 describe('wait-editor', () => {
-    let noErrorState;
-    beforeEach(() => {
-        noErrorState = {
-            name : {value: 'testWaitName', error: null},
-            label : {value: 'testWaitLabel', error: null},
-            description: 'wait description',
-            elementType : 'WAIT',
-            guid : '141f916fee-1c6f3-108bf-1ca54-16c041fcba152a7',
-            isCanvasElement : true,
-            locationX : 789,
-            locationY : 123,
-            waitEvents: [
-                {
-                    guid: 'waitEvent1',
-                    label: 'wait event label',
-                    name: 'waitEventName',
-                    conditions: [],
-                    conditionLogic: { value: CONDITION_LOGIC.AND, error: null },
-                }
-            ]
-        };
-    });
     it('handles the property changed event and updates the property', () => {
         const waitElement = createComponentForTest({node: noErrorState});
         const event = new PropertyChangedEvent('description', 'new desc', null);
@@ -104,6 +110,56 @@ describe('wait-editor', () => {
         getShadowRoot(waitElement).querySelector(selectors.WAIT_EVENT).dispatchEvent(event);
         return Promise.resolve().then(() => {
             expect(waitReducer).toHaveBeenCalledWith(waitElement.node, event);
+        });
+    });
+});
+
+describe('default path', () => {
+    it('handles DefaultPathChangedEvent and updates defaultConnectorLabel', () => {
+        const waitElement = createComponentForTest({node: noErrorState});
+        // trigger showing of default path
+        const reorderableWaitEventNav = getShadowRoot(waitElement).querySelector(selectors.REORDERABLE_NAV);
+        reorderableWaitEventNav.dispatchEvent(new CustomEvent('itemselected', {
+            detail: { itemId: DEFAULT_WAIT_EVENT_ID }
+        }));
+        return Promise.resolve().then(() => {
+            const defaultPathChangedEvent = new PropertyChangedEvent('defaultConnectorLabel', 'new label', null);
+            getShadowRoot(waitElement).querySelector(selectors.DEFAULT_PATH).dispatchEvent(defaultPathChangedEvent);
+
+            const mockCallParams = waitReducer.mock.calls[0];
+            const waitReducerEvent = mockCallParams[1];
+
+            expect(mockCallParams[0]).toEqual(noErrorState);
+            const expectedReducerEvent = {
+                type: 'propertychanged',
+                detail: {
+                    propertyName: 'defaultConnectorLabel',
+                    value: defaultPathChangedEvent.detail.value
+                }
+            };
+            expect(waitReducerEvent.type).toEqual(expectedReducerEvent.type);
+            expect(waitReducerEvent.detail.propertyName).toEqual(expectedReducerEvent.detail.propertyName);
+            expect(waitReducerEvent.detail.value).toEqual(expectedReducerEvent.detail.value);
+        });
+    });
+    it('initial default path does not have an error', () => {
+        const waitElement = createComponentForTest({node: noErrorState});
+        return Promise.resolve().then(() => {
+            const reorderableWaitEventsNav = getShadowRoot(waitElement).querySelector(selectors.REORDERABLE_NAV);
+            const menuItems = reorderableWaitEventsNav.menuItems;
+            expect(menuItems[1].hasErrors).toBeFalsy();
+        });
+    });
+    it('default path has an error if there is no label', () => {
+        noErrorState.defaultConnectorLabel = {
+            value: '',
+            error: 'Label should not be empty'
+        };
+        const waitElement = createComponentForTest({node: noErrorState});
+        return Promise.resolve().then(() => {
+            const reorderableWaitEventsNav = getShadowRoot(waitElement).querySelector(selectors.REORDERABLE_NAV);
+            const menuItems = reorderableWaitEventsNav.menuItems;
+            expect(menuItems[1].hasErrors).toBeTruthy();
         });
     });
 });
