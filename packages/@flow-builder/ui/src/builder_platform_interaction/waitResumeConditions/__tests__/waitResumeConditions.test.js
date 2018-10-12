d0939d30a5bf8c0875d1e00f86b47d28f1e2d088
Raji Srikantan <rsrikantan@salesforce.com>
2018-10-12 15:51:58 -0800

    [218] Automation: Wait resume conditions: Set the resumeEventType (alarm vs platform event) when loading an existing wait node https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005bBW0IAM/view @rev j.david, j.garciarovetta@ #review-16737526 @alikhar, @alejandrolopez, @aaron.liebling, @adeogirikar, @j.garciarovetta, @cnastasa, @j.david @markfixed@ BLT: CLI local=false
    @precheckin 16752356@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16753906]

--- packages/@flow-builder/ui/src/builder_platform_interaction/waitResumeConditions/__tests__/waitResumeConditions.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/waitResumeConditions/__tests__/waitResumeConditions.test.js
@@ -14,9 +14,120 @@ const createComponentUnderTest = (props) => {
 
 const selectors = {
     waitTimeEvent: 'builder_platform_interaction-wait-time-event',
+    waitPlatformEvent: 'builder_platform_interaction-wait-platform-event',
+    lightningRadioGroup: 'lightning-radio-group',
+};
+
+const resumeEventType = {
+    timeEventType: 'TIME_EVENT_TYPE',
+    platformEventType: 'PLATFORM_EVENT_TYPE'
+};
+
+const verifyResumeEventType = (waitResumeConditions, shouldShowTimeEvent) => {
+    const lightningRadioGroup = getShadowRoot(waitResumeConditions).querySelector(selectors.lightningRadioGroup);
+    const waitTimeEvent = getShadowRoot(waitResumeConditions).querySelector(selectors.waitTimeEvent);
+    const waitPlatformEvent = getShadowRoot(waitResumeConditions).querySelector(selectors.waitPlatformEvent);
+    if (shouldShowTimeEvent) {
+        expect(lightningRadioGroup.value).toBe(resumeEventType.timeEventType);
+        expect(waitTimeEvent).toBeTruthy();
+        expect(waitPlatformEvent).toBeNull();
+    } else {
+        expect(lightningRadioGroup.value).toBe(resumeEventType.platformEventType);
+        expect(waitTimeEvent).toBeNull();
+        expect(waitPlatformEvent).toBeTruthy();
+    }
 };
 
 describe('waitResumeConditions', () => {
+    describe('resume event type', () => {
+        it('is time event if absolute alarm is the event type', () => {
+            const props = {
+                eventType: WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME,
+                resumeTimeParameters: [],
+            };
+            verifyResumeEventType(createComponentUnderTest(props), true);
+        });
+
+        it('is time event if direct record alarm is the event type', () => {
+            const props = {
+                eventType: WAIT_TIME_EVENT_TYPE.DIRECT_RECORD_TIME,
+                resumeTimeParameters: [],
+            };
+            verifyResumeEventType(createComponentUnderTest(props), true);
+        });
+
+        it('is platform event if the event type with suffix __e', () => {
+            const props = {
+                eventType: 'MyEvent__e',
+                resumeTimeParameters: [],
+            };
+            verifyResumeEventType(createComponentUnderTest(props), false);
+        });
+
+        it('is platform event if the event type is any random string', () => {
+            const props = {
+                eventType: 'foo',
+                resumeTimeParameters: [],
+            };
+            verifyResumeEventType(createComponentUnderTest(props), false);
+        });
+    });
+
+    describe('toggle between resume event types', () => {
+        const dispatchChangeEvent = (waitResumeConditions, newType) => {
+            const lightningRadioGroup = getShadowRoot(waitResumeConditions).querySelector(selectors.lightningRadioGroup);
+            lightningRadioGroup.dispatchEvent(new CustomEvent('change', {detail: {value: newType}}));
+        };
+        it('sets the resume event type as platform event when switching to that', () => {
+            const props = {
+                eventType: WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME,
+                resumeTimeParameters: [],
+            };
+            const waitResumeConditions = createComponentUnderTest(props);
+            dispatchChangeEvent(waitResumeConditions, resumeEventType.platformEventType);
+            return Promise.resolve().then(() => {
+                verifyResumeEventType(waitResumeConditions, false);
+            });
+        });
+
+        describe('switching from platform event type to time event type', () => {
+            it('sets the resume event type as time event and resets to default event type', () => {
+                const props = {
+                    eventType: WAIT_TIME_EVENT_TYPE.DIRECT_RECORD_TIME,
+                    resumeTimeParameters: [],
+                };
+                const waitResumeConditions = createComponentUnderTest(props);
+                dispatchChangeEvent(waitResumeConditions, resumeEventType.platformEventType);
+                return Promise.resolve().then(() => {
+                    dispatchChangeEvent(waitResumeConditions, resumeEventType.timeEventType);
+                    return Promise.resolve().then(() => {
+                        verifyResumeEventType(waitResumeConditions, true);
+                        const waitTimeEvent = getShadowRoot(waitResumeConditions).querySelector(selectors.waitTimeEvent);
+                        expect(waitTimeEvent.eventType).toEqual(WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME);
+                    });
+                });
+            });
+
+            it('clears params', () => {
+                const props = {
+                    eventType: WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME,
+                    resumeTimeParameters: ['param1'],
+                };
+                const waitResumeConditions = createComponentUnderTest(props);
+                dispatchChangeEvent(waitResumeConditions, resumeEventType.platformEventType);
+                return Promise.resolve().then(() => {
+                    dispatchChangeEvent(waitResumeConditions, resumeEventType.timeEventType);
+                    return Promise.resolve().then(() => {
+                        verifyResumeEventType(waitResumeConditions, true);
+                        const waitTimeEvent = getShadowRoot(waitResumeConditions).querySelector(selectors.waitTimeEvent);
+                        expect(waitTimeEvent.eventType).toEqual(WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME);
+                        expect(waitTimeEvent.resumeTimeParameters).toEqual([]);
+                    });
+                });
+            });
+        });
+    });
+
     describe('wait time event', () => {
         let waitResumeConditions;
         let props;
@@ -38,6 +149,14 @@ describe('waitResumeConditions', () => {
         it('passes resumeTimeParameters to waitTimeEvent', () => {
             const waitTimeEvent = getShadowRoot(waitResumeConditions).querySelector(selectors.waitTimeEvent);
             expect(waitTimeEvent.resumeTimeParameters).toEqual(props.resumeTimeParameters);
+        });
+
+        it('changing the event type is tracked', () => {
+            waitResumeConditions.eventType = WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME;
+            return Promise.resolve().then(() => {
+                const waitTimeEvent = getShadowRoot(waitResumeConditions).querySelector(selectors.waitTimeEvent);
+                expect(waitTimeEvent.eventType).toEqual(WAIT_TIME_EVENT_TYPE.ABSOLUTE_TIME);
+            });
         });
     });
 });
