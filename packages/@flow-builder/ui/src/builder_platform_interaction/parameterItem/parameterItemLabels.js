6d8dcc242d7985cc8fe756a1aee443a448af0935
Antoine Magnier <amagnier@salesforce.com>
2018-10-25 08:33:00 -0800

    Fix missing alt text for IO param delete button. https://gus.lightning.force.com/a07B0000005wnX1IAI #review-16832101 @pham.nguyen @rev pham.nguyen@ BLT: CLI local=false
    @precheckin 16832172@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16832771]

--- packages/@flow-builder/ui/src/builder_platform_interaction/parameterItem/parameterItemLabels.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/parameterItem/parameterItemLabels.js
@@ -2,10 +2,12 @@ import toggleActive from "@salesforce/label/FlowBuilderActionCallEditor.toggleAc
 import toggleInactive from "@salesforce/label/FlowBuilderActionCallEditor.toggleInactive";
 import parameterPlaceholder from "@salesforce/label/FlowBuilderActionCallEditor.parameterPlaceholder";
 import warningHeaderForSummary from "@salesforce/label/FlowBuilderActionCallEditor.warningHeaderForSummary";
+import deleteAlternativeText from '@salesforce/label/FlowBuilderRows.deleteRowAlternativeText';
 
 export const LABELS = {
     toggleActive,
     toggleInactive,
     parameterPlaceholder,
     warningHeaderForSummary,
+    deleteAlternativeText
 };
