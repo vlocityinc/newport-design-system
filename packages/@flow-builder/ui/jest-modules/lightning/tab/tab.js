3889e777c07558bba284891c0395793c38e27ff0
Michael Coughlin <mcoughlin@salesforce.com>
2018-11-01 16:02:25 -0800

    Use value instead of id in left panel lightning-tabs Clean up help url Min width on strategy setup page
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005bINqIAM/view
    
    #review-16882796
    @rev dzak@
    BLT: CLI local=false
    
    @precheckin 16883159@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16884566]

--- packages/@flow-builder/ui/jest-modules/lightning/tab/tab.js
+++ packages/@flow-builder/ui/jest-modules/lightning/tab/tab.js
@@ -3,4 +3,6 @@ import { LightningElement, api } from 'lwc';
 export default class LightingTab extends LightningElement {
     @api loadContent = jest.fn();
     @api label = jest.fn();
+
+    @api value = jest.fn();
 }
