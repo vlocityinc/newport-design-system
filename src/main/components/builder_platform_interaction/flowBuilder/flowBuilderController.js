f504b5ef04c743f20308f99bae7f0bec3b8262ca
Michael Coughlin <mcoughlin@salesforce.com>
2018-11-07 13:08:55 -0800

    Customize Flow/Strategy Builders loading pages
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005XtKMIA0/view
    #review-16913491
    
    @rev sgesin@
    BLT: CLI local=false
    
    @precheckin 16917450@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16918683]

--- src/main/components/builder_platform_interaction/flowBuilder/flowBuilderController.js
+++ src/main/components/builder_platform_interaction/flowBuilder/flowBuilderController.js
@@ -1,6 +1,12 @@
 ({
     onInit : function(cmp, event, helper) {
         helper.init(cmp);
+
+        // hide the loading box
+        var loadingBoxes = document.querySelectorAll('.flow-builder-loading-box');
+        loadingBoxes.forEach(function(el) {
+            el.style.display = 'none';
+        });
     },
 
     createWelcomeMat : function(cmp, event, helper) {
