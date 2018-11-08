157046c9a728fa9b10eb002ba1c14d9884fcd20c
Michael Coughlin <mcoughlin@salesforce.com>
2018-11-08 14:54:56 -0800

    Replace loading machine image with black white screen
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005XtKMIA0/view
    
    #review-16926803
    @rev sgesin@
    @markfixed@
    BLT: CLI local=false
    
    @precheckin 16927463@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 16928727]

--- src/main/components/builder_platform_interaction/flowBuilder/flowBuilderController.js
+++ src/main/components/builder_platform_interaction/flowBuilder/flowBuilderController.js
@@ -1,12 +1,6 @@
 ({
     onInit : function(cmp, event, helper) {
         helper.init(cmp);
-
-        // hide the loading box
-        var loadingBoxes = document.querySelectorAll('.flow-builder-loading-box');
-        loadingBoxes.forEach(function(el) {
-            el.style.display = 'none';
-        });
     },
 
     createWelcomeMat : function(cmp, event, helper) {
