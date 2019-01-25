633abf0b1e7bffa8e2be9c57d0c2ac676b7cc9d2
Autointegration Service <autointeg@salesforce.com>
2019-01-24 19:24:54 -0800

    Autointegrate Change: [17350046] Fri Jan 25 01:39:36 2019 (app_218_patch_main) @rev autointegrate@ ---------------------------------------------------------- Change: [[17350046]] ddsouza@ABS-PreCheckinProcessing-ee80fe16-7527-4740-8fe4-cdab06461c75 on 2019/01/24 17:32:24
    Dynamic choice set test failure fix
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000005crlJIAQ/view
    @rev priya.mittal@
    @markfixed@
    BLT: CLI local=false
    
    @precheckin 17348987@
    @precheckin 17350088@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 17350562]

--- packages/@flow-builder/ui/src/builder_platform_interaction/elementFactory/__tests__/base/dynamicChoiceSet.test.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/elementFactory/__tests__/base/dynamicChoiceSet.test.js
@@ -21,7 +21,7 @@ const mockDynamicChoiceSetObjectForNoParam = {
     dataType: undefined,
     sortOrder: SORT_ORDER.NOT_SORTED
 };
-const mockParamForDynamicChoiceSet = {
+const mockDynamicChoiceSetWithNoLimitSortOrder = {
     limit: 0,
     displayField: 'mockDisplayField',
     valueField: 'mockValueField',
@@ -41,6 +41,13 @@ const mockDynamicChoiceSetResult = {
     valueField: 'mockValueField',
     dataType: 'mockDataType',
     sortOrder: SORT_ORDER.ASC
+};
+const mockDynamicChoiceSetResultWithNoLimitSortOrder = {
+    limit: '',
+    displayField: 'mockDisplayField',
+    valueField: 'mockValueField',
+    dataType: 'mockDataType',
+    sortOrder: SORT_ORDER.NOT_SORTED
 };
 const mockDyanmicChoiceMetadataObjectResult = {
     limit: 5,
@@ -64,9 +71,15 @@ describe('createDynamicChoiceSet', () => {
         });
     });
     describe('when valid mock dyanmic choice set element is sent as a param', () => {
-        const result = createDynamicChoiceSet(mockParamForDynamicChoiceSet);
+        const result = createDynamicChoiceSet(mockDynamicChoiceSetResult);
         it('result object matches the mockDynamicChoiceSetResult object', () => {
             expect(result).toMatchObject(mockDynamicChoiceSetResult);
+        });
+    });
+    describe('when mock dyanmic choice set element with no limit or sort order is sent as a param', () => {
+        const result = createDynamicChoiceSet(mockDynamicChoiceSetWithNoLimitSortOrder);
+        it('result object matches the mockDynamicChoiceSetResult object', () => {
+            expect(result).toMatchObject(mockDynamicChoiceSetResultWithNoLimitSortOrder);
         });
     });
 });
