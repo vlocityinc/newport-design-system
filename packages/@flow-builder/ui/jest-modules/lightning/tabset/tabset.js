da04bd313bde12f271ae543654285f096b3eb4c5
Autointegration Service <autointeg@salesforce.com>
2019-04-05 13:12:46 -0800

    Autointegrate Change: [17837191] Fri Apr  5 19:46:52 2019 (app_220_patch_main) @rev autointegrate@ ---------------------------------------------------------- Change: [[17837191]] gsmolyn@ABS-PreCheckinProcessing-edf43ce2-5d9d-4a64-bbab-b08c5b7ce78e on 2019/04/05 12:38:14
    [220][NBA] Fix test: missing some default fields
    
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000006S18KIAS/view
    
    #review-17836111 @cnastasa
    @rev cnastasa@
    @markfixed@
    @testfix@
    BLT: CLI local=false
    
    @precheckin 17836233@
    @precheckin 17837270@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 17838217]

--- packages/@flow-builder/ui/jest-modules/lightning/tabset/tabset.js
+++ packages/@flow-builder/ui/jest-modules/lightning/tabset/tabset.js
@@ -3,4 +3,5 @@ import { LightningElement, api } from 'lwc';
 export default class LightningTabSet extends LightningElement {
     @api title;
     @api variant = jest.fn();
+    @api activeTabValue;
 }
