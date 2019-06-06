68e13f0360c76cfeea6f942ddd483f44b7359815
Alex Trzeciak <atrzeciak@salesforce.com>
2019-06-05 17:20:01 -0800

    Refactor Reducer Prior to Autocomplete Work
    Acceptance Criteria:
    1). Apply Prettier
    2). Consolidate same cases
    3). Remove Object.assign for Object spread
    
    https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B0000006v8vcIAA/view
    
    @rev priya.mittal ankush.bansal ppominville@
    #review-18216119 @priya.mittal @ankush.bansal @ppominville 
    BLT: CLI local=false
    
    @precheckin 18225616@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 18226498]

--- packages/@flow-builder/ui/src/builder_platform_interaction/reducers/flowPropertiesReducer.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/reducers/flowPropertiesReducer.js
@@ -1,5 +1,16 @@
-import { UPDATE_FLOW, UPDATE_PROPERTIES, UPDATE_PROPERTIES_AFTER_SAVE_FAILED, UPDATE_PROPERTIES_AFTER_SAVING, UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_TEMPLATE, UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_PROCESS_TYPE, ADD_START_ELEMENT, SELECT_ON_CANVAS, TOGGLE_ON_CANVAS, DESELECT_ON_CANVAS } from "builder_platform_interaction/actions";
-import { createFlowProperties } from "builder_platform_interaction/elementFactory";
+import {
+    ADD_START_ELEMENT,
+    DESELECT_ON_CANVAS,
+    SELECT_ON_CANVAS,
+    TOGGLE_ON_CANVAS,
+    UPDATE_FLOW,
+    UPDATE_PROPERTIES,
+    UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_PROCESS_TYPE,
+    UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_TEMPLATE,
+    UPDATE_PROPERTIES_AFTER_SAVE_FAILED,
+    UPDATE_PROPERTIES_AFTER_SAVING
+} from 'builder_platform_interaction/actions';
+import { createFlowProperties } from 'builder_platform_interaction/elementFactory';
 /**
  * Reducer for properties
  * @param {Object} state properties object in the store
@@ -7,48 +18,57 @@ import { createFlowProperties } from "builder_platform_interaction/elementFactor
  * @return {Object} new state after reduction
  */
 
+const INIT = 'INIT';
 const flowProperties = createFlowProperties();
 
-export default function flowPropertiesReducer(state = flowProperties, action) {
-    switch (action.type) {
-        // Default value of hasUnsavedChanges is false in factory, so in this case it will be set to false.
-        case UPDATE_FLOW: return Object.assign({}, state, action.payload.properties, {
-            hasUnsavedChanges: false
-        });
-        // This action is dispatch when after flow properties are updated
-        // In first case, hasUnsavedChanges is set to true.
-        case UPDATE_PROPERTIES: return Object.assign({}, state, action.payload, {
-            hasUnsavedChanges: true
-        });
-        // This action is dispatched after flow save call is failed
-        // hasUnsavedChanges is set to true, so that the user can press save button again.
-        case UPDATE_PROPERTIES_AFTER_SAVE_FAILED: return Object.assign({}, state, action.payload, {
-            hasUnsavedChanges: true
-        });
-        // This action is dispatched after flow is saved
-        // After flow is saved, hasUnsavedChanges is set to false.
-        case UPDATE_PROPERTIES_AFTER_SAVING: return Object.assign({}, state, action.payload, {
-            hasUnsavedChanges: false
-        });
+/**
+ * Case Notes (Moved due to eslint errors)
+ * UPDATE_PROPERTIES_AFTER_SAVING: This action is dispatched after flow is saved.
+ * After flow is saved, hasUnsavedChanges is set to false.
+ * UPDATE_FLOW: Default value of hasUnsavedChanges is false in factory, so in this case it will be set to false.
+ * UPDATE_PROPERTIES_AFTER_SAVE_FAILED: This action is dispatched after flow save call is failed.
+ * hasUnsavedChanges is set to true, so that the user can press save button again.
+ * UPDATE_PROPERTIES: This action is dispatch when after flow properties are updated
+ * In first case, hasUnsavedChanges is set to true.
+ *
+ */
 
-        case UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_TEMPLATE: return Object.assign({}, state, action.payload, {
-            hasUnsavedChanges: true
-        });
-
-        case UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_PROCESS_TYPE: return Object.assign({}, state, action.payload, {
-            hasUnsavedChanges: false
-        });
-
-        // For these, hasUnsavedChanges will not be updated.
+export default function flowPropertiesReducer(
+    state = flowProperties,
+    { payload, type }
+) {
+    switch (type) {
+        case UPDATE_FLOW:
+            return {
+                ...state,
+                ...payload.properties,
+                hasUnsavedChanges: false
+            };
+        case UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_PROCESS_TYPE:
+        case UPDATE_PROPERTIES_AFTER_SAVING:
+            return {
+                ...state,
+                ...payload,
+                hasUnsavedChanges: false
+            };
+        case UPDATE_PROPERTIES_AFTER_CREATING_FLOW_FROM_TEMPLATE:
+        case UPDATE_PROPERTIES_AFTER_SAVE_FAILED:
+        case UPDATE_PROPERTIES:
+            return {
+                ...state,
+                ...payload,
+                hasUnsavedChanges: true
+            };
         case ADD_START_ELEMENT:
+        case DESELECT_ON_CANVAS:
+        case INIT:
         case SELECT_ON_CANVAS:
         case TOGGLE_ON_CANVAS:
-        case DESELECT_ON_CANVAS:
-        case 'INIT': return state;
-
-        // hasUnsavedChanges is set to true, if any other action is dispatch
-        default: return Object.assign({}, state, {
-            hasUnsavedChanges: true
-        });
+            return state;
+        default:
+            return {
+                ...state,
+                hasUnsavedChanges: true
+            };
     }
 }
