b24d45cf536f9d2b30937dff469ab703e0a1439e
Emy Paulson <epaulson@vlocity.com>
2019-07-23 20:21:00 +0530

    Minor fix

--- ui/components/action/action.stories.js
+++ ui/components/action/action.stories.js
@@ -13,22 +13,23 @@ storiesOf(`${base}`, module)
     .addDecorator(withDocs(notes))
     .add('Default', () => {
         return withExample(`<div class="nds-p-around_x-small nds-action">
-    <ul>
-        <li class="nds-item nds-display_inline">
-            <c-navigate-action data-name="My Action">
-                <slot>
-                    <a role="menuitem" tabindex="0" class="nds-action_item nds-p-horizontal_small">
-                        <div class="nds-action_icon nds-m-around_small">
-                            <c-icon><svg aria-hidden="true" class="nds-icon nds-m-around_xxx-small slds-m-right_small nds-icon-text-default nds-icon_small" style="fill: rgb(5, 166, 223);">
-                  <use
-                    xlink:href="#httpsemyv93devedlightningforcecomresource1559900061000emyv93newportassetsiconsutilityspritesvgsymbolssvg_displaytext">
+        <ul>
+          <li class="nds-item nds-action_inline">
+            <a role="menuitem"
+               tabindex="0"
+               class="nds-action_item nds-p-horizontal_small">
+              <div class="nds-action_icon nds-m-around_small">
+                <svg aria-hidden="true"
+                     class="nds-icon nds-m-around_xxx-small slds-m-right_small nds-icon-text-default nds-icon_small"
+                     style="fill: rgb(5, 166, 223);">
+                  <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#display_text">
                   </use>
-                </svg><span class="nds-assistive-text">My Action</span></c-icon>
-                        </div><span class="nds-action_text">My Action</span>
-                    </a>
-                </slot>
-            </c-navigate-action>
-        </li>
-    </ul>
-</div>`);
+                </svg>
+                <span class="nds-assistive-text">My Action</span>
+              </div>
+              <span class="nds-action_text">My Action</span>
+            </a>
+          </li>
+        </ul>
+      </div>`);
     });
