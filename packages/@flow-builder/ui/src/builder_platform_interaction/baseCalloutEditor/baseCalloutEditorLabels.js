5a9a25f7de456d675f0fb142bc85f54d50c8a37e
Aniko van der Lee <avanderlee@salesforce.com>
2019-11-13 14:52:50 -0800

    Adding description text to the dynamic type mapping section on the ActionCall prop editor https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07B00000077IamIAE/view @rev maleksandrov@ @markfixed@
    @precheckin 19755669@
    
    [git-p4: depot-paths = "//app/main/core/ui-interaction-builder-components/": change = 19757711]

--- packages/@flow-builder/ui/src/builder_platform_interaction/baseCalloutEditor/baseCalloutEditorLabels.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/baseCalloutEditor/baseCalloutEditorLabels.js
@@ -2,9 +2,10 @@
 import runInMode from '@salesforce/label/FlowBuilderSubflowEditor.runInMode';
 import learnMore from '@salesforce/label/FlowRunInMode.learnMore';
 import dataTypeMappingsHeader from '@salesforce/label/FlowBuilderCalloutEditor.dataTypeMappingsHeader';
-
+import dataTypeMappingsSectionDescription from '@salesforce/label/FlowBuilderCalloutEditor.dataTypeMappingsSectionDescription';
 export const LABELS = {
     runInMode,
     learnMore,
-    dataTypeMappingsHeader
+    dataTypeMappingsHeader,
+    dataTypeMappingsSectionDescription
 };
