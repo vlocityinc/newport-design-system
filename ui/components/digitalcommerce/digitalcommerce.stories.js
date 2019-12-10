9c6c391c39ed69c6190e8c0a0654fc68bb29dd98
Aminsab Abdulrahim Kodaganur <akodaganur@vlocity.com>
2019-12-10 12:00:47 +0530

    appliedPromotionList added to NDS

--- ui/components/digitalcommerce/digitalcommerce.stories.js
+++ ui/components/digitalcommerce/digitalcommerce.stories.js
@@ -255,4 +255,34 @@ storiesOf(`${base}`, module)
       </div>
     </div>
     `);
-  });
+  })
+  .add("dcAppliedPromotionList", () => {
+    return withExample(`
+      <article class="nds-dc-applied-promo-container">
+        <div class="nds-dc-applied-promo">
+          <div class="nds-dc-applied-promo-label">10percentOffer</div>
+          <div class="nds-dc-adjustment-value-one-time">
+            <span class="">
+              -100$
+            </span>
+          </div>
+          <div class="nds-dc-adjustment-value-recurring"></div>
+
+          <div class="nds-dc-delete-icon">
+            <svg class="nds-dc-tile_trash" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" fill="rgb(28, 102, 188);" viewBox="0 0 900.5 900.5" style="enable-background:new 0 0 900.5 900.5; height: .8rem" xml:space="preserve">
+              <g>
+                <path d="M176.415,880.5c0,11.046,8.954,20,20,20h507.67c11.046,0,20-8.954,20-20V232.487h-547.67V880.5L176.415,880.5z
+              M562.75,342.766h75v436.029h-75V342.766z M412.75,342.766h75v436.029h-75V342.766z M262.75,342.766h75v436.029h-75V342.766z"></path>
+                <path d="M618.825,91.911V20c0-11.046-8.954-20-20-20h-297.15c-11.046,0-20,8.954-20,20v71.911v12.5v12.5H141.874
+              c-11.046,0-20,8.954-20,20v50.576c0,11.045,8.954,20,20,20h34.541h547.67h34.541c11.046,0,20-8.955,20-20v-50.576
+              c0-11.046-8.954-20-20-20H618.825v-12.5V91.911z M543.825,112.799h-187.15v-8.389v-12.5V75h187.15v16.911v12.5V112.799z"></path>
+              </g>
+            </svg>
+            <span class="nds-dc-tile_trash-title nds-dc-trash-title">Remove</span>
+          </div>
+          <div class="nds-dc-edit-column"></div>
+        </div>
+        <div class="nds-dc-offer-group-separator nestedChild"></div>
+      </article>
+    `);
+  });;
