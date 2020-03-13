a8a4be9f7ca7c2e4b9f19b6612df016a910eceac
Patrice Pominville <ppominville@salesforce.com>
2020-03-12 19:20:59 -0700

    @W-7314162 @P4CL:21312227@ fix core (#202)
    * reverted locator changes
    
    * reverted locator changes
    
    * reverted locator changes
    
    * reverted locator changes
    
    * reverted locator changes

--- src/main/java/ui/interactionbuilder/components/configuration/UIInteractionBuilderComponentsAutoConfiguration.java
+++ src/main/java/ui/interactionbuilder/components/configuration/UIInteractionBuilderComponentsAutoConfiguration.java
@@ -1,4 +1,4 @@
-package ui.interaction.builder.components.configuration;
+package ui.interactionbuilder.components.configuration;
 
 import java.util.logging.Logger;
 import org.auraframework.adapter.ComponentLocationAdapter;
