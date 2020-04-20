e603ba8240a32144701ad1338e32b9a052c92a43
Aniko van der Lee <avanderlee@salesforce.com>
2020-04-20 16:30:30 -0700

    @W-7036751 Sections and Columns canvas work - fit and finish with feedback from UX (#401)
    * Aniko - Screen canvas work (WIP)
    
    * Aniko - fixing up unit tests and other cleanup
    
    * Aniko - Sections and Columns canvas: more test fixes
    
    * Aniko - cleanup of S&C canvas work based on feedback
    
    * Aniko - more cleanup
    
    * Aniko - more cleanup and added some missing tests
    
    * Aniko - adding a constant for the palette drag location
    
    * Aniko - Updating screenCanvas comments to be more accurate
    
    * Aniko - Some sections and columns canvas fit and finish
    
    * Aniko - force push

--- packages/@flow-builder/ui/src/builder_platform_interaction/screenSectionField/screenSectionField.js
+++ packages/@flow-builder/ui/src/builder_platform_interaction/screenSectionField/screenSectionField.js
@@ -9,9 +9,9 @@ export default class ScreenSectionField extends LightningElement {
     @api selectedItemGuid;
 
     // TODO: This approach for setting the slds_size_* of the columns won't work when the user is able to
-    // set the widths of the various columns (rather than all columns being equal width) - W-7207695
+    // set the widths of the various columns (rather than all columns being equal width) - W-7207695.
     get columnClass() {
-        let calculatedColumnClass = 'slds-grow slds-col slds-p-horizontal_x-small';
+        let calculatedColumnClass = 'slds-grow slds-col screen-section-field-column';
         if (this.section && this.section.fields && this.section.fields.length > 0) {
             calculatedColumnClass = calculatedColumnClass + ' slds-size_1-of-' + this.section.fields.length;
         }
