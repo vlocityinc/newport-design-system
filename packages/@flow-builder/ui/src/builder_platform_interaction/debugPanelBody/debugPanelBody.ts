ee4927e4b6702c31e5f5115f6b69e6f576a7901f
Joshua Huang <joshua.huang@salesforce.com>
2020-07-10 13:54:13 -0700

    Jhuang/regex and error body update1 (#645)
    * regex fix and icon
    
    * update error element box
    
    * reformat code to avoid redundant checks
    
    * remove redundant check
    
    * resolve changes in other files
    
    * add space to null regex
    
    * update comments for readability
    
    * pom update
    
    * resolve console key error

--- packages/@flow-builder/ui/src/builder_platform_interaction/debugPanelBody/debugPanelBody.ts
+++ packages/@flow-builder/ui/src/builder_platform_interaction/debugPanelBody/debugPanelBody.ts
@@ -1,10 +1,23 @@
 import { LightningElement, api } from 'lwc';
 import { LABELS } from './debugPanelBodyLabels';
+import { generateGuid } from 'builder_platform_interaction/storeLib';
 
-const NULL = 'null';
+/**
+ * This is the debug panel body that shows the debug run info inside each accordion on
+ * builder canvas (debugMode)
+ *
+ * - Splits rawText by newline characters
+ * - Strings with "Error Occurred:" or "Error element" recieve an error box
+ * - Warning icon appears when a variable is null or (null) and "Failed to find record"
+ * - Governor limit info recieves a "Governor Limit Info" title
+ * - Titles are "Result" or any string that ends with ":"
+ */
+
+const NULL = ' null$';
+const NULLPARENS = '(null)';
 const NEWLINE = '\\n';
 
-// Labels with "{0}" or "{1}" need to be replaced"
+/* Labels with "{0}" or "{1}" need to be replaced" for proper checking */
 const ERROR = LABELS.faultMess.replace('{0}', '');
 const DML = LABELS.dml.replace(/\{0\}|\{1\}/, '[0-9]+');
 const DMLROW = LABELS.dmlRow.replace(/\{0\}|\{1\}/, '[0-9]+');
@@ -14,88 +27,97 @@ const ERRBODY = LABELS.errorBody.replace(/ \{0\} \(\{1\}\)./, '').trim();
 
 export default class debugPanelBody extends LightningElement {
     @api rawText;
+
     @api title;
 
     get textObj() {
         return this.getDebugInfoBody();
     }
 
+    /**
+     * Main function to format the string. Each string will be marked by booleans that
+     * indicate if it will be need special formatting
+     * - Splits text by newline
+     * - Removes /" characters from output
+     * - Removes trailing whitespace to help with checking for titles
+     */
     getDebugInfoBody() {
         const obj = [{}];
-        let splitText = this.rawText.split(NEWLINE);
+        const splitText = this.rawText.split(NEWLINE);
         let needsGovTitle = true;
 
-        if (splitText[0] === '') {
-            splitText = splitText.slice(1);
+        if (this.title.includes(ERRBODY)) {
+            obj.push(this.formatErrorBody(splitText));
+            return obj;
         }
+
         for (let i = 0; i < splitText.length; i++) {
-            // Get rid of \" in strings
             let curr = splitText[i].replace(/\\"/g, '');
-            // Trim ending whitespace (for bolding purposes)
             curr = curr.trim();
 
-            // Check for "Error Occurred:" string
             if (curr.includes(ERROR)) {
                 obj.push(this.formatErrorMessage(curr));
-                // Keep out empty strings, breadcrumb, titles
-            } else if (curr.includes(ERRBODY)) {
-                obj.push(this.formatErrorBody(curr, i, splitText));
-                // After error message, no other strings remain
-                break;
             } else if (curr !== '' && !curr.includes('$$:') && curr !== this.title) {
-                const temp = { value: curr, isTitle: false, isWarn: false };
-                // Check for bold or gov limit info
+                const temp = { value: curr, isTitle: false, isWarn: false, id: generateGuid() };
                 if (curr === LABELS.resultLabel || curr === LABELS.inputLabel) {
                     temp.isTitle = true;
                 } else if (curr.charAt(curr.length - 1) === ':') {
-                    // remove ":" at the end
                     temp.value = curr.slice(0, -1);
                     temp.isTitle = true;
-                } else if (curr.includes(NULL) || curr === LABELS.failedFind) {
-                    // for now, inserting warning icon whenver there is "null" and for failed to find
+                } else if (curr.match(NULL) || curr.includes(NULLPARENS) || curr === LABELS.failedFind) {
                     temp.isWarn = true;
                 } else if (needsGovTitle && this.isGovLimit(curr)) {
-                    // Only need one title
                     needsGovTitle = false;
                     obj.push({ value: LABELS.govInfo, isTitle: true, isWarn: false });
                 }
                 obj.push(temp);
             }
         }
+        obj[0] = { value: '', isTitle: false, isWarn: false, id: generateGuid() };
         return obj;
     }
 
-    // for errors that start with "Error Occurred:"
+    /**
+     * Handles error messages that begin with "Error Occurred:"
+     * - All text is placed in an error box
+     * - "Error Occurred:" portion is set as the title
+     */
     formatErrorMessage(currString) {
-        // Bold "Error Occurred:" part, leave the rest
         const boldEnd = currString.indexOf(':') + 1;
         return {
             bold: currString.substring(0, boldEnd),
             value: currString.substring(boldEnd),
             isTitle: false,
             isWarn: false,
-            isError: true
+            isError: true,
+            id: generateGuid()
         };
     }
 
-    formatErrorBody(currString, index, splitText) {
+    /**
+     * Handles error messages with a header that begins with "Error element"
+     * - All text is placed in an error box
+     * - "Error Occurred:" portion is added as the title inside the error box
+     */
+    formatErrorBody(splitText) {
         const temp = {
-            value: currString,
+            bold: ERROR,
+            value: '',
             isTitle: false,
             isWarn: false,
-            isError: true
+            isError: true,
+            id: generateGuid()
         };
-        // Error message may include other strings in array
-        for (let i = index + 1; i < splitText.length; i++) {
-            if (i !== '') {
+        for (let i = 0; i < splitText.length; i++) {
+            if (splitText[i] !== '') {
                 temp.value = temp.value + '\n' + splitText[i];
             }
         }
         return temp;
     }
 
+    /* Checks if a string is a gov limit info */
     isGovLimit(currString) {
-        // check for gov limit info
         return currString.match(DML) || currString.match(DMLROW) || currString.match(SOQL) || currString.match(SOQLROW);
     }
 }
