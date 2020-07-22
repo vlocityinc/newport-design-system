f71cf4a4eff4fb58c51acc86ee8fbd0c79c07ff6
Derek D'Souza <ddsouza@salesforce.com>
2020-07-22 13:59:30 -0700

    @W-7735620 Merging 226 platform event label changes (#704)
    * Merging 226 platform event label changes
    
    * Pom update

--- packages/@flow-builder/ui/src/builder_platform_interaction/platformEventEditor/platformEventEditorLabels.ts
+++ packages/@flow-builder/ui/src/builder_platform_interaction/platformEventEditor/platformEventEditorLabels.ts
@@ -1,8 +1,10 @@
 // @ts-nocheck
+import platformEventDescription from '@salesforce/label/FlowBuilderStartEditor.platformEventDescription';
 import platformEventEnitityObject from '@salesforce/label/FlowBuilderStartEditor.platformEventEnitityObject';
 import platformEventEnitityObjectPlaceholder from '@salesforce/label/FlowBuilderStartEditor.platformEventEnitityObjectPlaceholder';
 
 export const LABELS = {
+    platformEventDescription,
     platformEventEnitityObject,
     platformEventEnitityObjectPlaceholder
 };
