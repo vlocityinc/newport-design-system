524b6d1a5111e94a1cd18469d89e2172f98a04fb
Matt Wu <mawu@vlocity.com>
2020-07-30 11:24:34 -0700

    Creating lwc branch

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,20 @@
-import { addParameters, addDecorator } from "@storybook/html";
-import { withDesignTokens } from "../scripts/storybook/design-tokens";
-import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
+import { addParameters, addDecorator } from '@storybook/html';
+import { withDesignTokens } from '../scripts/storybook/design-tokens';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
-import { setCustomElements } from "@storybook/web-components";
-import customElements from "../custom-elements.json";
+import { setCustomElements } from '@storybook/web-components';
+import customElements from '../custom-elements.json';
 
 setCustomElements(customElements);
 
-import "../assets/styles/index.css";
+import '../assets/styles/index.css';
 
 addDecorator(withDesignTokens);
-//addDecorator(withA11y);
+// addDecorator(withA11y);
 
 addParameters({
   options: {
-    name: "Newport Design System",
+    name: 'Newport Design System',
     isFullScreen: false,
     showPanel: true,
     hierarchySeparator: /\//,
--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,123 +1,123 @@
-import "@lwc/synthetic-shadow";
-import { createElement } from "lwc";
-import Button from "c/button";
+import '@lwc/synthetic-shadow';
+import { createElement } from 'lwc';
+import Button from 'c/button';
 
 // import PropTypes from 'prop-types';
 
 export default {
-  title: "c/button",
-  component: "c-button",
-  attribute: "variant",
+  title: 'c/button',
+  component: 'button',
+  attribute: 'variant',
   parameters: {
-    componentSubtitle: "Buttons are clickable items used to perform an action.",
+    componentSubtitle: 'Buttons are clickable items used to perform an action.',
     // Using storyDescription here applies one description to all stories--is overridden by story parameter storyDescription
     docs: {
       storyDescription:
-        "Buttons are clickable items used to perform an action.",
+        'Buttons are clickable items used to perform an action.',
     },
   },
 };
 
 export const Neutral = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Neutral Button";
-  button.theme = "nds";
-  button.variant = "neutral";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Neutral Button';
+  button.theme = 'nds';
+  button.variant = 'neutral';
   return button;
 };
 
 Neutral.story = {
   parameters: {
     docs: {
-      storyDescription: "Neutral button description here.",
+      storyDescription: 'Neutral button description here.',
     },
   },
 };
 
 export const Brand = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Brand Button";
-  button.theme = "nds";
-  button.variant = "brand";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Brand Button';
+  button.theme = 'nds';
+  button.variant = 'brand';
   return button;
 };
 
 Brand.story = {
   parameters: {
     docs: {
-      storyDescription: "Brand button description here.",
+      storyDescription: 'Brand button description here.',
     },
   },
 };
 
 export const OutlineBrand = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Outline Brand Button";
-  button.theme = "nds";
-  button.variant = "outline-brand";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Outline Brand Button';
+  button.theme = 'nds';
+  button.variant = 'outline-brand';
   return button;
 };
 
 OutlineBrand.story = {
   parameters: {
     docs: {
-      storyDescription: "Outline Brand button description here.",
+      storyDescription: 'Outline Brand button description here.',
     },
   },
 };
 
 export const Destructive = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Destructive Button";
-  button.theme = "nds";
-  button.variant = "destructive";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Destructive Button';
+  button.theme = 'nds';
+  button.variant = 'destructive';
   return button;
 };
 
 Destructive.story = {
   parameters: {
     docs: {
-      storyDescription: "Destructive button description here.",
+      storyDescription: 'Destructive button description here.',
     },
   },
 };
 
 export const TextDestructive = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Text Destructive Button";
-  button.theme = "nds";
-  button.variant = "text-destructive";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Text Destructive Button';
+  button.theme = 'nds';
+  button.variant = 'text-destructive';
   return button;
 };
 
 TextDestructive.story = {
   parameters: {
     docs: {
-      storyDescription: "Text Destructive button description here.",
+      storyDescription: 'Text Destructive button description here.',
     },
   },
 };
 
 export const Success = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Success button";
-  button.theme = "nds";
-  button.variant = "success";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Success button';
+  button.theme = 'nds';
+  button.variant = 'success';
   return button;
 };
 
 Success.story = {
   parameters: {
     docs: {
-      storyDescription: "Success button description here.",
+      storyDescription: 'Success button description here.',
     },
   },
 };
 
 export const Disabled = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Disabled button";
-  button.theme = "nds";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Disabled button';
+  button.theme = 'nds';
   button.disabled = 1;
   return button;
 };
@@ -125,71 +125,71 @@ export const Disabled = () => {
 Disabled.story = {
   parameters: {
     docs: {
-      storyDescription: "Success button description here.",
+      storyDescription: 'Success button description here.',
     },
   },
 };
 
 export const Reset = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Reset button";
-  button.theme = "nds";
-  button.type = "reset";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Reset button';
+  button.theme = 'nds';
+  button.type = 'reset';
   return button;
 };
 
 Reset.story = {
   parameters: {
     docs: {
-      storyDescription: "Reset button description here.",
+      storyDescription: 'Reset button description here.',
     },
   },
 };
 
 export const Submit = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Submit button";
-  button.theme = "nds";
-  button.type = "submit";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Submit button';
+  button.theme = 'nds';
+  button.type = 'submit';
   return button;
 };
 
 Submit.story = {
   parameters: {
     docs: {
-      storyDescription: "Submit button description here.",
+      storyDescription: 'Submit button description here.',
     },
   },
 };
 
 export const Icon = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Icon button";
-  button.theme = "nds";
-  button.iconName = "utility:close";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Icon button';
+  button.theme = 'nds';
+  button.iconName = 'utility:close';
   return button;
 };
 
 Icon.story = {
   parameters: {
     docs: {
-      storyDescription: "Icon button description here.",
+      storyDescription: 'Icon button description here.',
     },
   },
 };
 
 export const RightAlligned = () => {
-  const button = createElement("c-button", { is: Button });
-  button.label = "Right button";
-  button.theme = "nds";
-  button.iconPosition = "right";
+  const button = createElement('c-button', { is: Button });
+  button.label = 'Right button';
+  button.theme = 'nds';
+  button.iconPosition = 'right';
   return button;
 };
 
 RightAlligned.story = {
   parameters: {
     docs: {
-      storyDescription: "Right alligned button description here.",
+      storyDescription: 'Right alligned button description here.',
     },
   },
 };
