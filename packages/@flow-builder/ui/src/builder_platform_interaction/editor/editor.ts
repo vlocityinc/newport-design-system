7bc5ff587b68c2cb21415f180a21a62b1754eae3
Derek D'Souza <ddsouza@salesforce.com>
2020-08-06 19:18:20 -0700

    Hide Debug Spinner on JS exceptions (#736)

--- packages/@flow-builder/ui/src/builder_platform_interaction/editor/editor.ts
+++ packages/@flow-builder/ui/src/builder_platform_interaction/editor/editor.ts
@@ -898,37 +898,42 @@ export default class Editor extends LightningElement {
         fetch(
             SERVER_ACTION_TYPE.RUN_DEBUG,
             ({ data, error }) => {
-                if (error) {
-                    // Handle server exception here if something is needed beyond our automatic server error popup
-                } else {
-                    // Setup the debug data object for the debug panel, and switch to debug mode
-                    const endInterviewTime = new Date();
-                    this.builderMode = BUILDER_MODE.DEBUG_MODE;
-                    const interviewStatus = data && data[0] && data[0].interviewStatus;
-                    const debugTrace = data && data[0] && data[0].debugTrace;
-                    const debugError = data && data[0] && data[0].errors;
-                    this.debugData = {
-                        interviewStatus,
-                        debugTrace,
-                        error: debugError,
-                        startInterviewTime,
-                        endInterviewTime
-                    };
-                    // Highlight connectors on the canvas if no errors in the debug run, and no unsaved changes in the current flow
-                    if (!debugError && !this.properties.hasUnsavedChanges) {
-                        const canvasDecorator = data[1];
-                        if (canvasDecorator) {
-                            const connectorsToHighlight = getConnectorsToHighlight(canvasDecorator);
-                            storeInstance.dispatch(decorateCanvas({ connectorsToHighlight }));
-                        }
+                try {
+                    if (error) {
+                        // Handle server exception here if something is needed beyond our automatic server error popup
                     } else {
-                        // Else, clear any existing highlights on the canvas
-                        storeInstance.dispatch(clearCanvasDecoration);
+                        // Setup the debug data object for the debug panel, and switch to debug mode
+                        const endInterviewTime = new Date();
+                        this.builderMode = BUILDER_MODE.DEBUG_MODE;
+                        const interviewStatus = data && data[0] && data[0].interviewStatus;
+                        const debugTrace = data && data[0] && data[0].debugTrace;
+                        const debugError = data && data[0] && data[0].errors;
+                        this.debugData = {
+                            interviewStatus,
+                            debugTrace,
+                            error: debugError,
+                            startInterviewTime,
+                            endInterviewTime
+                        };
+                        // Highlight connectors on the canvas if no errors in the debug run, and no unsaved changes in the current flow
+                        if (!debugError && !this.properties.hasUnsavedChanges) {
+                            const canvasDecorator = data[1];
+                            if (canvasDecorator) {
+                                const connectorsToHighlight = getConnectorsToHighlight(canvasDecorator);
+                                storeInstance.dispatch(decorateCanvas({ connectorsToHighlight }));
+                            }
+                        } else {
+                            // Else, clear any existing highlights on the canvas
+                            storeInstance.dispatch(clearCanvasDecoration);
+                        }
+                        this.clearUndoRedoStack();
                     }
-                    this.clearUndoRedoStack();
+                    this.spinners.showDebugSpinner = false;
+                    hidePopover();
+                } catch (e) {
+                    this.spinners.showDebugSpinner = false;
+                    throw e;
                 }
-                this.spinners.showDebugSpinner = false;
-                hidePopover();
             },
             {
                 flowDevName: storeInstance.getCurrentState().properties.name,
