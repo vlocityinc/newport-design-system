a6c8ddd2169ead227c667ad856b4a3c2dfb19141
Matthew Goldspink <mgoldspink@salesforce.com>
2020-10-08 13:16:43 +0100

    Updating version numbers

--- .storybook/config.js
+++ .storybook/config.js
@@ -24,7 +24,7 @@ addDecorator(withDesignTokens);
 
 addParameters({
   options: {
-    name: 'Newport Design System (ins-109.0)',
+    name: 'Newport Design System (cme-109.1)',
     isFullScreen: false,
     showPanel: true,
     hierarchySeparator: /\//,
