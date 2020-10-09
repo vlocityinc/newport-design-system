fce755e2aed2333a2e21f94f6a1edfef32e08c67
kinsey <kinseyschell@gmail.com>
2020-10-09 10:11:36 +0200

    clean up selectors hero

--- ui/molecules/communities-hero/communities-hero.stories.js
+++ ui/molecules/communities-hero/communities-hero.stories.js
@@ -32,10 +32,9 @@ return withExample(`
             <div>Get personalized service from a local agent</div>
         </div>
     </div>
-    <div class="nds-hero-container">
-        <img class="nds-hero nds-hero-large" alt="Cityscape"
-            src="assets/images/communities/cityscape.svg" title="Community Hero" />
-        <img class="nds-hero nds-hero-small" alt="Cityscape" src="assets/images/communities/cityscape-mobile.svg"
+    div class="nds-hero">
+        <img class="nds-hero_large" alt="Cityscape" src="assets/images/communities/cityscape.svg" title="Community Hero" />
+        <img class="nds-hero_small" alt="Cityscape" src="assets/images/communities/cityscape-mobile.svg"
             title="Community Hero" />
     </div>
 </div>
