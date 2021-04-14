7ea7514be790c90f360e3ecd10c0484751e23fed
Raji Srikantan <rsrikantan@salesforce.com>
2021-04-13 17:10:51 -0700

    add a label for the step exit determination flow selector (#1573)

--- packages/@flow-builder/ui/src/builder_platform_interaction/stageStepEditor/stageStepEditorLabels.ts
+++ packages/@flow-builder/ui/src/builder_platform_interaction/stageStepEditor/stageStepEditorLabels.ts
@@ -16,6 +16,7 @@ import exitSectionLabel from '@salesforce/label/FlowBuilderStageStepEditor.exitS
 import exitActionSelectionLabel from '@salesforce/label/FlowBuilderStageStepEditor.exitActionSelectionLabel';
 import basedOnCustomFlow from '@salesforce/label/FlowBuilderStageStepEditor.basedOnCustomFlow';
 import determinationFlowSelectorHeader from '@salesforce/label/FlowBuilderStageStepEditor.determinationFlowSelectorHeader';
+import exitDeterminationFlowSelectorHeader from '@salesforce/label/FlowBuilderStageStepEditor.exitDeterminationFlowSelectorHeader';
 import criteriaBasedOnStepHelpText from '@salesforce/label/FlowBuilderStageStepEditor.criteriaBasedOnStepHelpText';
 import criteriaActionHelpText from '@salesforce/label/FlowBuilderStageStepEditor.criteriaActionHelpText';
 
@@ -66,6 +67,7 @@ export const LABELS = {
     basedOnCustomFlow,
     exitActionSelectionLabel,
     determinationFlowSelectorHeader,
+    exitDeterminationFlowSelectorHeader,
     criteriaBasedOnStepHelpText,
     criteriaActionHelpText,
     recordSelectionSectionLabel,
