918c410c8c292cc7ef868ede162c1ed1de51c937
Simeng (Sylvie) Hua <s.hua@salesforce.com>
2021-10-08 12:43:08 -0700

    @W-9773429 Replace ForEach and AfterLast hard coded labels in alcConnectorLabels.ts @P4CL:34309311@  (#2131)
    * replaced hard coded labels for loop
    
    * updated labels

--- packages/@flow-builder/auto-layout-canvas-ui/src/builder_platform_interaction/alcConnector/alcConnectorLabels.ts
+++ packages/@flow-builder/auto-layout-canvas-ui/src/builder_platform_interaction/alcConnector/alcConnectorLabels.ts
@@ -1,14 +1,13 @@
 import addElementIconAltText from '@salesforce/label/AlcConnector.addElementIconAltText';
 import faultConnectorBadgeLabel from '@salesforce/label/AlcConnector.faultConnectorBadgeLabel';
 import connectorButtonLabel from '@salesforce/label/AlcConnector.connectorButtonLabel';
-
-const forEachBadgeLabel = 'For Each';
-const afterLastBadgeLabel = 'After Last';
+import forEachBadgeLabel from '@salesforce/label/FlowBuilderConnectorLabels.forEachBadgeLabel';
+import afterLastBadgeLabel from '@salesforce/label/FlowBuilderConnectorLabels.afterLastBadgeLabel';
 
 export const LABELS: Labels = {
     addElementIconAltText,
     faultConnectorBadgeLabel,
+    connectorButtonLabel,
     forEachBadgeLabel,
-    afterLastBadgeLabel,
-    connectorButtonLabel
+    afterLastBadgeLabel
 };
