ce3b06b24d57143c42f3b9b78ad80bba0f7d7bef
Matthew Goldspink <mgoldspink@salesforce.com>
2022-02-17 12:31:50 +0000

    Revert "Merge branch 'master' into v234"
    This reverts commit ca780b31fa728edfb68d184c091b8181c13bc921, reversing
    changes made to 46bb5266947faadbb2d72d73d88181866e470bad.

--- docs/docs.stories.js
+++ docs/docs.stories.js
@@ -4,7 +4,6 @@ import gettingStarted from './getting-started.md';
 import introToNewport from './introduction-to-newport.md';
 import buildDeploy from './build-deploy.md';
 import customizeAButton from './customizing-a-button.md';
-import otherPublishedVersions from './other-published-versions.md';
 import { renderAsMarkdown } from '..././../../scripts/storybook';
 
 storiesOf(`${base}`.replace(/(^\/)|(\/$)/g, ''),  module)
@@ -20,7 +19,4 @@ storiesOf(`${base}`.replace(/(^\/)|(\/$)/g, ''),  module)
   })
   .add('How to customize a button', () => {
     return customizeAButton;
-  })
-  .add('Looking for a different version?', () => {
-    return otherPublishedVersions;
   });
