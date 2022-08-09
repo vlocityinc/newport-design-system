de5cc5b6b15b2065ee3bcaca66962e1f41178c8a
Franck Thomas <franck.thomas@salesforce.com>
2022-08-09 14:31:36 +0200

    @W-11554375 remove SFCI fallback (Nucleus on STMFB) @skip-ci@ (#2968)

--- scripts/check-core-version.sh
+++ scripts/check-core-version.sh
@@ -36,40 +36,38 @@ fi
 
 TAG=$(p4 print //app/$BRANCH/core/$BAZEL_RELATIVE_PATH@$CL | grep $JAR_NAME | grep -Eo $RELEASE_VERSION_REGEXP)
 
-
 echo "\nBranch: ${LIGHT_GRAY}$BRANCH${NO_COLOR}"
 echo "Latest ui-interaction-builder-components jar version: ${GREEN}$TAG${NO_COLOR}"
 echo "Latest P4 changelist: ${BLUE}$CL ($SWARM_CHANGES_BASE_URL$CL)${NO_COLOR}"
 echo "Nexus URL: $NEXUS_JAR_BASE_URL$TAG/\n"
 
 NUCLEUS_TAG="v${TAG}"
-SFCI_TAG="sfci-${TAG}"
 
 echo "${LIGHT_GRAY}Related Git release commit details:${NO_COLOR}"
 COMMIT=$(git show --format="$GIT_SHOW_FORMAT" -s "${NUCLEUS_TAG}" | sed -n '/Author:/,$p')
 if [  -z "$COMMIT" ]; then
-    echo "${RED}FALLBACK: Trying to fetch commit for release tag: $SFCI_TAG as $NUCLEUS_TAG does not exist for branch $BRANCH ${NO_COLOR}"
-    COMMIT=$(git show --format="$GIT_SHOW_FORMAT" -s "${SFCI_TAG}" | sed -n '/Author:/,$p')
-fi
-COMMIT_HASH=$(echo "$COMMIT" | grep -Eo $COMMIT_HASH_REGEXP)
-PR_ID=$(echo "$COMMIT" | grep -Eo $PR_ID_REGEXP)
-GUS_WORK_ITEM=$(echo "$COMMIT" | grep -Eo $GUS_WI_REGEXP)
-# We do retrieve the origin Git URL and remove the trailing .git part
-GIT_URL=$(git remote get-url origin | rev | cut -c5- | rev)
-
-
-echo "$COMMIT"
-echo "Commit URL: $GIT_URL/commit/$COMMIT_HASH"
-if [ -z $PR_ID ]; then
-    echo "${CYAN}No PR ID provided in the commit message${NO_COLOR}"
+    echo "${RED}Unable to fetch Git commit details for release tag: $NUCLEUS_TAG on branch $BRANCH ${NO_COLOR}\n"
 else
-    echo "PR URL: $GIT_URL/pull/${PR_ID//#/}" # removing # char from grabbed Pull Request ID
-fi
+    COMMIT_HASH=$(echo "$COMMIT" | grep -Eo $COMMIT_HASH_REGEXP)
+    PR_ID=$(echo "$COMMIT" | grep -Eo $PR_ID_REGEXP)
+    GUS_WORK_ITEM=$(echo "$COMMIT" | grep -Eo $GUS_WI_REGEXP)
+    # We do retrieve the origin Git URL and remove the trailing .git part
+    GIT_URL=$(git remote get-url origin | rev | cut -c5- | rev)
 
-if [ -z $GUS_WORK_ITEM ]; then
-    echo "${CYAN}No WorkItem provided in the commit message\n${NO_COLOR}"
-else
-    echo "GUS WorkItem: $GUS_WORK_ITEM_BASE_URL$GUS_WORK_ITEM\n"
+
+    echo "$COMMIT"
+    echo "Commit URL: $GIT_URL/commit/$COMMIT_HASH"
+    if [ -z $PR_ID ]; then
+        echo "${CYAN}No PR ID provided in the commit message${NO_COLOR}"
+    else
+        echo "PR URL: $GIT_URL/pull/${PR_ID//#/}" # removing # char from grabbed Pull Request ID
+    fi
+
+    if [ -z $GUS_WORK_ITEM ]; then
+        echo "${CYAN}No WorkItem provided in the commit message\n${NO_COLOR}"
+    else
+        echo "GUS WorkItem: $GUS_WORK_ITEM_BASE_URL$GUS_WORK_ITEM\n"
+    fi
 fi
 
 
